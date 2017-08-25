d7f82317489e6843917fe6794ed2c524c589cb3f
JeanBilheux <bilheuxjm@ornl.gov>
2017-08-25 16:41:36 -0400

    Work in progress providing ui to define layers of ImagingReso

---
+++ __code/ui_builder.py
@@ -0,0 +1,22 @@
+import os
+
+class UiBuilder(object):
+
+    pyuic = ''
+    
+    def __init__(self, ui_name=''):
+        self.define_pyuic_to_run()
+        self.ui_name = os.path.abspath('ui/' + ui_name)
+        [base, ext] = os.path.splitext(ui_name)
+        py_name = base + '.py'
+        self.py_name = os.path.abspath('__code/' + py_name)
+
+        # run command
+        os.system(self.pyuic + ' ' + self.ui_name + ' -o ' + self.py_name)
+        
+    def define_pyuic_to_run(self):
+        try:
+            from PyQt4 import QtGui
+            self.pyuic = 'pyuic4'
+        except:
+            self.pyuic = 'pyuic5'
