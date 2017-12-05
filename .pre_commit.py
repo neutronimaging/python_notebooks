d680187d351a357e1da7247cf057476efea66200
JeanBilheux <bilheuxjm@ornl.gov>
2017-12-05 14:12:18 -0500

    Improved script that pre-comming the notebook. This way, I will be able to see if a notebook has been removed by mistake

--- .pre_commit.py
+++ .pre_commit.py
@@ -5,10 +5,22 @@ import glob
 
 _file_path = os.path.dirname(__file__)
 list_notebooks = glob.glob(os.path.abspath(_file_path) + '/*.ipynb')
+list_notebooks.sort()
 
-print("Cleaned ...")
+print("Cleaning ...")
 for _notebook in list_notebooks:
     os.system('nbstripout {}'.format(_notebook))
     print(" > {}".format(_notebook))
+
+list_notebooks_after = glob.glob(os.path.abspath(_file_path) + '/*.ipynb')
+list_notebooks_after.sort()
+
+if len(list_notebooks) == len(list_notebooks_after):
+    print(" Cleaning Result:  OK!")
+else:
+    print(" Cleaning Result: WARNNG!")
+    for _file in list_notebook:
+        if not (_file in list_notebooks_after):
+            print(" Missing File: {}".format(_file))
 
 
