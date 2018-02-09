2e6b22a84e82f15bf9542b17fb29e935ef2c80de
JeanBilheux <bilheuxjm@ornl.gov>
2018-02-09 12:07:11 -0500

    Fixed typo in config file

--- __code/config.py
+++ __code/config.py
@@ -2,5 +2,5 @@ debugging = False
 debugger_username = 'j35'
 debugger_folder = ['/Volumes/my_book_thunderbolt_duo/IPTS/', '/Users/j35/IPTS/', '/']
 project_folder = 'IPTS_16259'
-system_folder = '/HFIR/CG1DI'
+system_folder = '/HFIR/CG1D'
 analysis_machine = 'cg1d-analysis2.ornl.gov'
