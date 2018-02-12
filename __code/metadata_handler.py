b15617d32c53d52e2f50968a4e439672d8323010
JeanBilheux <bilheuxjm@ornl.gov>
2018-02-12 12:28:11 -0500

    Added infos tab and implemented profile with fix range

--- __code/metadata_handler.py
+++ __code/metadata_handler.py
@@ -11,11 +11,11 @@ class MetadataHandler(object):
             try:
                 o_image = Image.open(file_name)
                 o_dict = dict(o_image.tag_v2)
-                time_stamp = o_dict[650000][0]
-                # time_stamp_s = str(o_dict[65002][0])
-                # time_stamp_ns = str(o_dict[65003][0])
-                # time_stamp_string = "{}.{}".format(time_stamp_s, time_stamp_ns)
-                # time_stamp = float(time_stamp_string)
+                #time_stamp = o_dict[650000][0]
+                time_stamp_s = str(o_dict[650002][0])
+                time_stamp_ns = str(o_dict[650003][0])
+                time_stamp_string = "{}.{}".format(time_stamp_s, time_stamp_ns)
+                time_stamp = float(time_stamp_string)
             except:
                 time_stamp = os.path.getmtime(file_name)
         elif ext == 'fits':
--- __code/water_intake_profile_calculator.py
+++ __code/water_intake_profile_calculator.py
@@ -38,10 +38,12 @@ from __code.ui_water_intake_profile  import Ui_MainWindow as UiMainWindow
 class WaterIntakeProfileSelector(QMainWindow):
 
     list_data = []
+    dict_data = {}
     current_image = []
     ignore_first_image_checked = True
     roi_width = 0.01
     roi = {'x0': 0, 'y0': 0, 'width': 200, 'height': 200}
+    table_column_width = [350, 150, 200]
 
     def __init__(self, parent=None, dict_data={}):
 
@@ -53,11 +55,13 @@ class WaterIntakeProfileSelector(QMainWindow):
         self.ui.setupUi(self)
         self.setWindowTitle("Profile Selector")
 
+        self.dict_data = dict_data
         self.list_data = dict_data['list_data'][1:]
         self._init_pyqtgraph()
         self._init_widgets()
         self.update_image()
         self.update_plots()
+        self.update_infos_tab()
 
     def _init_pyqtgraph(self):
         area = DockArea()
@@ -120,6 +124,43 @@ class WaterIntakeProfileSelector(QMainWindow):
         self.ui.file_index_slider.setValue(2)
         self.update_labels()
 
+        # update size of table columns
+        nbr_columns = self.ui.tableWidget.columnCount()
+        for _col in range(nbr_columns):
+            self.ui.tableWidget.setColumnWidth(_col, self.table_column_width[_col])
+
+    def update_infos_tab(self):
+        dict_data = self.dict_data
+
+        list_files = dict_data['list_images']
+        list_time_stamp = dict_data['list_time_stamp']
+        list_time_stamp_user = dict_data['list_time_stamp_user_format']
+
+        for _row, _file_name in enumerate(list_files):
+            _short_name = os.path.basename(_file_name)
+            _time_stamp_unix = list_time_stamp[_row]
+            _time_stamp_user = list_time_stamp_user[_row]
+
+            self.__insert_information_in_table(row=_row,
+                                               col0=_short_name,
+                                               col1=_time_stamp_unix,
+                                               col2=_time_stamp_user)
+
+    def __insert_information_in_table(self, row, col0, col1, col2):
+        self.ui.tableWidget.insertRow(row)
+
+        #col0
+        _item = QtGui.QTableWidgetItem(str(str(col0)))
+        self.ui.tableWidget.setItem(row, 0, _item)
+
+        #col1
+        _item = QtGui.QTableWidgetItem(str(str(col1)))
+        self.ui.tableWidget.setItem(row, 1, _item)
+
+        # col2
+        _item = QtGui.QTableWidgetItem(str(str(col2)))
+        self.ui.tableWidget.setItem(row, 2, _item)
+
     def update_labels(self):
         _roi = self.roi
         _x0 = _roi['x0']
@@ -181,6 +222,9 @@ class WaterIntakeProfileSelector(QMainWindow):
     def export_water_intake_clicked(self):
         pass
 
+    def display_imported_files_clicked(self):
+        print("displaying imported files")
+
     def roi_moved(self):
         region = self.roi_id.getArraySlice(self.current_image, self.ui.image_view.imageItem)
 
@@ -198,9 +242,23 @@ class WaterIntakeProfileSelector(QMainWindow):
         self.update_labels()
         self.update_profile_plot()
 
+    def _clean_image(self, image):
+        _result_inf = np.where(np.isinf(image))
+        image[_result_inf] = np.NaN
+        return image
+
+    def _force_range(self, image):
+        range_max = 1.2
+        _result_range = np.where(image > range_max)
+        image[_result_range] = np.NaN
+        return image
+
     def update_image(self):
         index_selected = self.ui.file_index_slider.value()
         _image = self.list_data[index_selected-1]
+        _image = np.transpose(_image)
+        _image = self._clean_image(_image)
+        _image = self._force_range(_image)
         self.current_image = _image
         self.ui.image_view.setImage(_image)
 
@@ -210,7 +268,6 @@ class WaterIntakeProfileSelector(QMainWindow):
         self.update_profile_plot()
 
     def profile_algo_changed(self):
-        print("profile algo changed")
         self.update_profile_plot()
 
     def help_button_clicked(self):
@@ -262,7 +319,7 @@ class WaterIntakeProfileCalculator(object):
     # Helper functions
     def __load_files(self):
         o_load = Normalization()
-        o_load.load(file=self.dict_time_stamp['list_images'], notebook=True)
+        o_load.load(file=self.dict_files['list_images'], notebook=True)
         self.dict_files['list_data'] = o_load.data['sample']['data']
 
     def __sort_files_using_time_stamp(self, dict_time_stamp):
@@ -284,9 +341,9 @@ class WaterIntakeProfileCalculator(object):
         sorted_list_time_stamp = time_stamp[sort_index]
         sorted_list_time_stamp_user_format = time_stamp_user_format[sort_index]
 
-        self.dict_time_stamp = {'list_images': list(sorted_list_images),
-                                'list_time_stamp': sorted_list_time_stamp,
-                                'list_time_stamp_user_format': sorted_list_time_stamp_user_format}
+        self.dict_files = {'list_images': list(sorted_list_images),
+                           'list_time_stamp': sorted_list_time_stamp,
+                           'list_time_stamp_user_format': sorted_list_time_stamp_user_format}
 
 
 
@@ -299,209 +356,3 @@ class WaterIntakeProfileCalculator(object):
 
 
 
-
-
-
-    ### OLD FILE
-
-    def select_counts_vs_time_files(self):
-        self.list_files_ui = ipywe.fileselector.FileSelectorPanel(instruction='Select Counts vs Time Files ...',
-                                                              start_dir=self.working_dir,
-                                                              multiple=True)
-
-        self.list_files_ui.show()
-
-    def retrieve_metadata(self, file_name):
-        """
-        return a dictionary of the metadata (plus number of comment lines)
-        """
-        with open(file_name) as f:
-            _metadata = {}
-            _nbr_comment_line = 0
-            for line in f:
-                li = line.strip()
-                if li.startswith("#"):
-                    _nbr_comment_line += 1
-                    m = re.match(r"^#(?P<tag_name>[,\(\)\s\w]*):(?P<tag_value>.*)", li)
-                    if m is None:
-                        continue
-                    _metadata[m.group('tag_name')] = m.group('tag_value').strip()
-            _metadata['nbr_comment_line'] = _nbr_comment_line
-            return _metadata
-
-    def retrieve_data(self, file_name, nbr_comment_line=0, col_names=None):
-        file_0 = pd.read_csv(file_name,
-                             skiprows=nbr_comment_line,
-                             header=None,
-                             names=col_names)
-        return file_0
-
-    def format_col_names(self, col_names):
-        _col_names = [_col.strip() for _col in col_names]
-        return _col_names
-
-    def retrieve_data_and_metadata(self):
-
-        self.list_files = self.list_files_ui.selected
-
-        w = widgets.IntProgress()
-        w.max = len(self.list_files)
-        display(w)
-        index = 0
-
-        files_data = {}
-        files_metadata = {}
-        for index, file in enumerate(self.list_files):
-            _metadata = self.retrieve_metadata(file)
-            files_metadata[file] = _metadata
-            _col_names = _metadata['Label'].split(',')
-            _new_col_names = self.format_col_names(_col_names)
-            _data = self.retrieve_data(file,
-                                      nbr_comment_line=_metadata['nbr_comment_line'],
-                                      col_names=_new_col_names)
-            files_data[file] = _data
-
-            index += 1
-            w.value = index
-
-        self.files_data = files_data
-        self.files_metadata = files_metadata
-
-        w.close()
-
-    def calculate_water_intake_peak(self):
-
-        pixel_index = self.files_data[self.list_files[0]]
-        nbr_pixel = len(pixel_index)
-
-        water_intake_peak = []
-        time_stamps = []
-        for _index, _file in enumerate(self.list_files):
-            counts = self.files_data[_file]['counts'].values
-            delta_array = []
-            for _pixel in range(0, nbr_pixel - 1):
-                _o_range = MeanRangeCalculation(data=counts)
-                _o_range.calculate_left_right_mean(pixel=_pixel)
-                _o_range.calculate_delta_mean_square()
-                delta_array.append(_o_range.delta_square)
-
-            time_stamps.append(self.files_metadata[_file]['Delta Time (s)'])
-
-            _peak_value = delta_array.index(max(delta_array[0:nbr_pixel - 5]))
-            water_intake_peak.append(_peak_value)
-
-        self.water_intake_peak = water_intake_peak
-        self.time_stamps = time_stamps
-
-    def display_water_intake_vs_profile(self):
-
-        self.retrieve_data_and_metadata()
-        self.calculate_water_intake_peak()
-
-        water_intake_peak = self.water_intake_peak
-        list_files = self.list_files
-        metadata = self.files_metadata
-
-        def display_intake_vs_profile(file_index):
-
-            plt.figure(figsize=(5,5))
-            ax_img = plt.subplot(111)
-            _file = list_files[file_index]
-            _data = self.files_data[_file]['counts'].values
-            _peak = float(water_intake_peak[file_index])
-            _metadata = metadata[_file]
-            _rebin = float(_metadata['Rebin in y direction'])
-            _metadata_entry = _metadata['ROI selected (y0, x0, height, width)']
-            m = re.match(r"\((?P<y0>\d*), (?P<x0>\d*), (?P<height>\d*), (?P<width>\d*)\)",
-                         _metadata_entry)
-            y0 = float(m.group('y0'))
-            height = float(m.group('height'))
-
-            _real_peak = (_peak + y0) * _rebin
-            _real_axis = np.arange(y0, height, _rebin)
-
-            ax_img.plot(_real_axis, _data, '.')
-            plt.title(os.path.basename(_file))
-            ax_img.axvline(x=_real_peak, color='r')
-
-        _ = interact(display_intake_vs_profile,
-                     file_index = widgets.IntSlider(min=0,
-                                                    max=len(list_files)-1,
-                                                    value=0,
-                                                    description='File Index'))
-
-    def display_water_intake_peak_in_px(self):
-
-
-        fig = plt.figure(figsize=(5, 5))
-        plt.plot(self.time_stamps, self.water_intake_peak)
-        plt.xlabel("Delta Time (s)")
-        plt.ylabel("Peak Position (pixel)")
-        plt.title("Peak vs Delta Time (s)")
-
-    def define_pixel_size(self):
-
-        box = widgets.HBox([widgets.Label("Size/Pixel Ratio"),
-                            widgets.FloatText(value=1,
-                                              step=0.1,
-                                              layout=widgets.Layout(width='10%')),
-                            widgets.Label("mm/px")])
-
-        display(box)
-        self.ratio_widget = box.children[1]
-
-    def display_water_intake_peak_in_mm(self):
-
-        water_intake_peak_px = np.array(self.water_intake_peak)
-        self.water_intake_peak_mm = water_intake_peak_px * self.ratio_widget.value
-
-        fig = plt.figure(figsize=(5, 5))
-        plt.plot(self.time_stamps, self.water_intake_peak_mm)
-        plt.xlabel("Delta Time (s)")
-        plt.ylabel("Peak Position (mm)")
-        plt.title("Peak vs Delta Time (s)")
-
-    def select_output_folder(self):
-        self.output_folder_ui = ipywe.fileselector.FileSelectorPanel(instruction='Select Output Folder ...',
-                                                                     start_dir=self.working_dir,
-                                                                     type='directory')
-        self.output_folder_ui.show()
-
-    def export(self):
-
-        output_folder = os.path.abspath(self.output_folder_ui.selected)
-        short_file_name = os.path.basename(os.path.abspath(os.path.dirname(self.list_files[0]))) + '.txt'
-        output_file_name = os.path.join(output_folder, short_file_name)
-
-        ascii_text = "# Source data: {}\n".format(os.path.basename(os.path.dirname(self.list_files[0])))
-        ascii_text += '# time_stamp(s), water_intake_position (mm)\n'
-        for _index in range(len(self.time_stamps)):
-            _x_value = self.time_stamps[_index]
-            _y_value = self.water_intake_peak_mm[_index]
-            ascii_text += "{}, {}\n".format(_x_value, _y_value)
-
-        make_ascii_file_from_string(text=ascii_text, filename=output_file_name)
-
-        display(HTML('<span style="font-size: 20px; color:blue">File created: ' +
-                     os.path.basename(output_file_name) + '<br> ' +
-                     'In folder: ' + os.path.dirname(output_file_name) + '</span>'))
-
-
-class MeanRangeCalculation(object):
-    '''
-    Mean value of all the counts between left_pixel and right pixel
-    '''
-
-    def __init__(self, data=None):
-        self.data = data
-        self.nbr_pixel = len(self.data)
-
-    def calculate_left_right_mean(self, pixel=-1):
-        _data = self.data
-        _nbr_pixel = self.nbr_pixel
-
-        self.left_mean = np.mean(_data[0:pixel+1])
-        self.right_mean = np.mean(_data[pixel+1:_nbr_pixel])
-
-    def calculate_delta_mean_square(self):
-        self.delta_square = np.square(self.left_mean - self.right_mean)
