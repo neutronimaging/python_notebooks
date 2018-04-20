4ff09a43d682d1f280796458730d032af947a211
JeanBilheux <bilheuxjm@ornl.gov>
2018-04-20 12:42:24 -0400

    started to build ui and connect widgets

--- __code/registration_profile.py
+++ __code/registration_profile.py
@@ -16,28 +16,64 @@ from __code.ui_registration_profile import Ui_MainWindow as UiMainWindowProfile
 
 class RegistrationProfileUi(QMainWindow):
 
+    data_dict = None
+
     def __init__(self, parent=None, data_dict=None):
 
-        QMainWindow.__init__(self, parent=None)
+        QMainWindow.__init__(self, parent=parent)
         self.ui = UiMainWindowProfile()
         self.ui.setupUi(self)
         self.setWindowTitle("Registration Profile Tool")
 
-        if self.parent:
+        self.init_widgets()
+
+        self.parent = parent
+        if parent:
             self.data_dict = self.parent.data_dict
         elif data_dict:
             display(HTML('<span style="font-size: 20px; color:blue">Check UI that poped up \
                 (maybe hidden behind this browser!)</span>'))
-            self.data_dict = self.data_dict
+            self.data_dict = data_dict
         else:
             raise ValueError("please provide data_dict")
 
+    def init_widgets(self):
+        # no need to show save and close if not called from parent UI
+        if self.parent is None:
+            self.ui.save_and_close_button.setVisible(False)
 
+    ## Event Handler
 
+    def help_button_clicked(self):
+        import webbrowser
+        webbrowser.open("https://neutronimaging.pages.ornl.gov/en/tutorial/notebooks/registration/")
 
+    def slider_file_changed(self, value):
+        pass
 
+    def previous_image_button_clicked(self):
+        print("previous")
 
+    def next_image_button_clicked(self):
+        pass
 
-    def closeEvent(self, c):
+    def registered_all_images_button_clicked(self):
+        print("registered all images")
+
+    def cancel_button_clicked(self):
         if self.parent:
             self.parent.registration_profile_ui = None
+        self.close()
+
+    def save_and_close_button_clicked(self):
+        """save registered images back to the main UI"""
+        self.cancel_button_clicked()
+
+    def opacity_slider_moved(self, value):
+        print("opacity slider")
+
+    def closeEvent(self, c):
+        print("here")
+        if self.parent:
+            self.parent.registration_profile_ui = None
+        self.close()
