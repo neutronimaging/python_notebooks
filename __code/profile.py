59ca00fd028af1eec3aae2917bb98ab8c006bfe8
JeanBilheux <bilheuxjm@ornl.gov>
2018-05-16 13:23:23 -0400

    Simplify the way the guide and profile rois are implemented and modified

--- __code/profile.py
+++ __code/profile.py
@@ -432,163 +432,12 @@ class ProfileUi(QMainWindow):
 
     def add_guide_and_profile_pyqt_roi(self, row=-1):
         """add the pyqtgraph roi guide and profiles"""
-        if row == -1:
-            row = 0
-
-        # guide
-        guide_roi = pg.RectROI([self.default_guide_roi['x0'], self.default_guide_roi['y0']],
-                                [self.default_guide_roi['width'], self.default_guide_roi['height']],
-                            pen=self.default_guide_roi['color_activated'])
-        guide_roi.addScaleHandle([1, 1], [0, 0])
-        guide_roi.addScaleHandle([0, 0], [1, 1])
-        guide_roi.sigRegionChanged.connect(self.guide_changed)
-        self.ui.image_view.addItem(guide_roi)
-        self.list_guide_pyqt_roi.insert(row, guide_roi)
-
-        # profile
-        [x0, y0, width, height] = self.get_item_row(row=row)
-        _profile_width = self.get_profile_width(row=row)
-        is_x_profile_direction = self.ui.profile_direction_x_axis.isChecked()
-        delta_profile = (_profile_width - 1) / 2.
-        if is_x_profile_direction:
-
-            profile_center = y0 + np.abs(np.int((height)/2.))
-
-            y_top = profile_center - delta_profile
-            y_bottom = profile_center + delta_profile
-
-            x_left = x0
-            x_right = x0 + width
-
-            pos = []
-            pos.append([x_left, y_top])
-            pos.append([x_right, y_top])
-            adj = []
-            adj.append([0, 1])
-
-            if y_top != y_bottom: # height == 1
-                pos.append([x_left, y_bottom])
-                pos.append([x_right, y_bottom])
-                adj.append([2, 3])
-
-            adj = np.array(adj)
-            pos = np.array(pos)
-
-        else: # y-profile direction
-
-            profile_center = x0 + np.abs(np.int((width) / 2.))
-
-            x_left = profile_center - delta_profile
-            x_right = profile_center + delta_profile
-
-            y_top = y0
-            y_bottom = y0 + height
-
-            pos = []
-            pos.append([x_left, y_top])
-            pos.append([x_left, y_bottom])
-            adj = []
-            adj.append([0, 1])
-
-            if y_top != y_bottom:  # height == 1
-                pos.append([x_right, y_top])
-                pos.append([x_right, y_bottom])
-                adj.append([2, 3])
-
-            adj = np.array(adj)
-            pos = np.array(pos)
-
-        line_color = self.profile_color
-        _list_line_color = list(line_color)
-        line_color = tuple(_list_line_color)
-        lines = np.array([line_color for n in np.arange(len(pos))],
-                         dtype=[('red', np.ubyte), ('green', np.ubyte),
-                                ('blue', np.ubyte), ('alpha', np.ubyte),
-                                ('width', float)])
-
-        profile = pg.GraphItem()
-        self.ui.image_view.addItem(profile)
-        profile.setData(pos=pos,
-                     adj=adj,
-                     pen=lines,
-                     symbol=None,
-                     pxMode=False)
-
-        self.list_profile_pyqt_roi.insert(row, profile)
+        o_profile = GuideAndProfileRoisHandler(parent=self, row=row)
+        o_profile.add()
 
     def update_profile_pyqt_roi(self, row=-1):
-
-        # profile
-        [x0, y0, width, height] = self.get_item_row(row=row)
-        _profile_width = self.get_profile_width(row=row)
-        is_x_profile_direction = self.ui.profile_direction_x_axis.isChecked()
-        delta_profile = (_profile_width - 1) / 2.
-        if is_x_profile_direction:
-
-            profile_center = y0 + np.abs(np.int((height) / 2.))
-
-            y_top = profile_center - delta_profile
-            y_bottom = profile_center + delta_profile
-
-            x_left = x0
-            x_right = x0 + width
-
-            pos = []
-            pos.append([x_left, y_top])
-            pos.append([x_right, y_top])
-            adj = []
-            adj.append([0, 1])
-
-            if y_top != y_bottom:  # height == 1
-                pos.append([x_left, y_bottom])
-                pos.append([x_right, y_bottom])
-                adj.append([2, 3])
-
-            adj = np.array(adj)
-            pos = np.array(pos)
-
-        else:  # y-profile direction
-
-            profile_center = x0 + np.abs(np.int((width) / 2.))
-
-            x_left = profile_center - delta_profile
-            x_right = profile_center + delta_profile
-
-            y_top = y0
-            y_bottom = y0 + height
-
-            pos = []
-            pos.append([x_left, y_top])
-            pos.append([x_left, y_bottom])
-            adj = []
-            adj.append([0, 1])
-
-            if y_top != y_bottom:  # height == 1
-                pos.append([x_right, y_top])
-                pos.append([x_right, y_bottom])
-                adj.append([2, 3])
-
-            adj = np.array(adj)
-            pos = np.array(pos)
-
-        line_color = self.profile_color
-        _list_line_color = list(line_color)
-        line_color = tuple(_list_line_color)
-        lines = np.array([line_color for n in np.arange(len(pos))],
-                         dtype=[('red', np.ubyte), ('green', np.ubyte),
-                                ('blue', np.ubyte), ('alpha', np.ubyte),
-                                ('width', float)])
-
-        profile = pg.GraphItem()
-        self.ui.image_view.addItem(profile)
-        profile.setData(pos=pos,
-                        adj=adj,
-                        pen=lines,
-                        symbol=None,
-                        pxMode=False)
-
-        self.ui.image_view.removeItem(self.list_profile_pyqt_roi[row])
-        self.list_profile_pyqt_roi[row] = profile
+        o_profile = GuideAndProfileRoisHandler(parent=self, row=row)
+        o_profile.update()
 
     def insert_row(self, row=-1):
         if row == -1:
@@ -1037,4 +886,108 @@ class ProfileUi(QMainWindow):
 
     def closeEvent(self, event=None):
         pass
+
+    
+class GuideAndProfileRoisHandler(object):
+
+    __profile = None
+
+    def __init__(self, parent=None, row=-1):
+        self.parent = parent
+        self.row = row
+        if self.row == -1:
+            self.row = 0
+
+    def add(self):
+        self._define_guide()
+        self._define_profile()
+        self.parent.list_profile_pyqt_roi.insert(self.row, self.__profile)
+
+    def update(self):
+        self._define_profile()
+        self.parent.ui.image_view.removeItem(self.parent.list_profile_pyqt_roi[self.row])
+        self.parent.list_profile_pyqt_roi[self.row] = self.__profile
+
+    def _define_guide(self):
+        """define the guide"""
+        guide_roi = pg.RectROI([self.parent.default_guide_roi['x0'], self.parent.default_guide_roi['y0']],
+                                [self.parent.default_guide_roi['width'], self.parent.default_guide_roi['height']],
+                            pen=self.parent.default_guide_roi['color_activated'])
+        guide_roi.addScaleHandle([1, 1], [0, 0])
+        guide_roi.addScaleHandle([0, 0], [1, 1])
+        guide_roi.sigRegionChanged.connect(self.parent.guide_changed)
+        self.parent.ui.image_view.addItem(guide_roi)
+        self.parent.list_guide_pyqt_roi.insert(self.row, guide_roi)
+
+    def _define_profile(self):
+        # profile
+        [x0, y0, width, height] = self.parent.get_item_row(row=self.row)
+        _profile_width = self.parent.get_profile_width(row=self.row)
+        is_x_profile_direction = self.parent.ui.profile_direction_x_axis.isChecked()
+        delta_profile = (_profile_width - 1) / 2.
+        if is_x_profile_direction:
+
+            profile_center = y0 + np.abs(np.int((height)/2.))
+
+            y_top = profile_center - delta_profile
+            y_bottom = profile_center + delta_profile
+
+            x_left = x0
+            x_right = x0 + width
+
+            pos = []
+            pos.append([x_left, y_top])
+            pos.append([x_right, y_top])
+            adj = []
+            adj.append([0, 1])
+
+            if y_top != y_bottom: # height == 1
+                pos.append([x_left, y_bottom])
+                pos.append([x_right, y_bottom])
+                adj.append([2, 3])
+
+            adj = np.array(adj)
+            pos = np.array(pos)
+
+        else: # y-profile direction
+
+            profile_center = x0 + np.abs(np.int((width) / 2.))
+
+            x_left = profile_center - delta_profile
+            x_right = profile_center + delta_profile
+
+            y_top = y0
+            y_bottom = y0 + height
+
+            pos = []
+            pos.append([x_left, y_top])
+            pos.append([x_left, y_bottom])
+            adj = []
+            adj.append([0, 1])
+
+            if y_top != y_bottom:  # height == 1
+                pos.append([x_right, y_top])
+                pos.append([x_right, y_bottom])
+                adj.append([2, 3])
+
+            adj = np.array(adj)
+            pos = np.array(pos)
+
+        line_color = self.parent.profile_color
+        _list_line_color = list(line_color)
+        line_color = tuple(_list_line_color)
+        lines = np.array([line_color for n in np.arange(len(pos))],
+                         dtype=[('red', np.ubyte), ('green', np.ubyte),
+                                ('blue', np.ubyte), ('alpha', np.ubyte),
+                                ('width', float)])
+
+        profile = pg.GraphItem()
+        self.parent.ui.image_view.addItem(profile)
+        profile.setData(pos=pos,
+                     adj=adj,
+                     pen=lines,
+                     symbol=None,
+                     pxMode=False)
+
+        self.__profile = profile
 
