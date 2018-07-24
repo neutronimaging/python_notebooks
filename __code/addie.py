27642dd2a9ebe4d5ebcfa5cba31c04431e54d8f9
JeanBilheux <bilheuxjm@ornl.gov>
2018-07-24 16:47:45 -0400

    Started to work on QTreeWidget

--- __code/addie.py
+++ __code/addie.py
@@ -6,7 +6,7 @@ try:
     from PyQt4 import QtCore, QtGui
     from PyQt4.QtGui import QMainWindow
 except ImportError:
-    from PyQt5.QtWidgets import QFileDialog
+    from PyQt5.QtWidgets import QFileDialog, QTreeWidgetItem
     from PyQt5 import QtCore, QtGui
     from PyQt5.QtWidgets import QApplication, QMainWindow
 
@@ -25,6 +25,37 @@ class Interface(QMainWindow):
         self.ui = UiMainWindow()
         self.ui.setupUi(self)
         self.setWindowTitle("Template Addie")
+
+        self.init_tree()
+
+    def init_tree(self):
+        # fill the self.ui.treeWidget
+        self.addItems(self.ui.treeWidget.invisibleRootItem())
+
+    def addItems(self, parent):
+        column = 0
+        sample = self.addParent(parent, column, "Sample", "Sample text")
+        vanadium = self.addParent(parent, column, "Vanadium", "Vanadium text")
+
+        self.addChild(sample, column, "Runs", "run")
+        self.addChild(sample, column, "Background", "backgorund")
+
+        self.addChild(vanadium, column, "Runs", "run")
+        self.addChild(vanadium, column, "Background", "backgorund")
+
+
+    def addParent(self, parent, column, title, data):
+        item = QTreeWidgetItem(parent, [title])
+        item.setData(column, QtCore.Qt.UserRole, data)
+        item.setChildIndicatorPolicy(QTreeWidgetItem.ShowIndicator)
+        item.setExpanded(True)
+        return item
+
+    def addChild(self, parent, column, title, data):
+        item = QTreeWidgetItem(parent, [title])
+        item.setData(column, QtCore.Qt.UserRole, data)
+        item.setCheckState(column, QtCore.Qt.Unchecked)
+        return item
 
     def init_widgets(self):
         pass
