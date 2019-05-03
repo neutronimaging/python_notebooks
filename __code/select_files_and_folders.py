1559a71a48ef47afe9738eef39a2dce4dda308a7
JeanBilheux <bilheuxjm@ornl.gov>
2019-05-03 16:56:12 -0400

    Improved output folder selection tool. This refs #1

--- __code/select_files_and_folders.py
+++ __code/select_files_and_folders.py
@@ -1,5 +1,8 @@
+import os
 from IPython.display import display
+from ipywidgets import widgets
 from IPython.core.display import HTML
+import ipywe.fileselector
 
 from __code.file_folder_browser import FileFolderBrowser
 from __code.utilities import display_html_message
@@ -22,6 +25,56 @@ class SelectFiles(FileFolderBrowser):
 
     def retrieve_list_of_files(self, list_of_files=''):
         self.list_of_files = list_of_files
+
+
+class SelectFolderWithDefaultPaths(FileFolderBrowser):
+
+    def __init__(self, system=None, next_function=None):
+        working_dir = system.System.get_working_dir()
+
+        super(SelectFolderWithDefaultPaths, self).__init__(working_dir=working_dir,
+                                                           next_function=next_function)
+
+        ipts = os.path.basename(self.working_dir)
+
+        button_layout = widgets.Layout(width='30%',
+                                       border='1px solid gray')
+
+        hbox = widgets.HBox([widgets.Button(description="Jump to {} Shared Folder".format(ipts),
+                                            button_style='success',
+                                            layout=button_layout),
+                             widgets.Button(description="Jump to My Home Folder",
+                                            button_style='success',
+                                            layout=button_layout)])
+        go_to_shared_button_ui = hbox.children[0]
+        go_to_home_button_ui = hbox.children[1]
+
+        go_to_shared_button_ui.on_click(self.display_file_selector_from_shared)
+        go_to_home_button_ui.on_click(self.display_file_selector_from_home)
+
+        display(hbox)
+
+        self.display_file_selector()
+
+    def display_file_selector_from_shared(self, ev):
+        start_dir = os.path.join(self.working_dir, 'shared')
+        self.output_folder_ui.remove()
+        self.display_file_selector(start_dir=start_dir)
+
+    def display_file_selector_from_home(self, ev):
+        import getpass
+        _user = getpass.getuser()
+        start_dir = os.path.join('/SNS/users', _user)
+        self.output_folder_ui.remove()
+        self.display_file_selector(start_dir=start_dir)
+
+    def display_file_selector(self, start_dir=''):
+        self.output_folder_ui = ipywe.fileselector.FileSelectorPanel(instruction='Select Output Folder',
+                                                                start_dir=start_dir,
+                                                                multiple=False,
+                                                                next=self.next_function,
+                                                                type='directory')
+        self.output_folder_ui.show()
 
 
 class SelectFolder(FileFolderBrowser):
