02474d31342887001410a1757f8825498237d195
JeanBilheux <bilheuxjm@ornl.gov>
2019-11-24 09:06:09 -0500

    Implemented algorithm that merge the images

--- __code/sequential_combine_images_using_metadata.py
+++ __code/sequential_combine_images_using_metadata.py
@@ -177,13 +177,14 @@ class SequentialCombineImagesUsingMetadata(object):
                 _previous_metadata = _current_metadata
                 _previous_run = _current_run
 
+            progress_bar.value = _index+1
+
         else:
             tag_name = "{}{}".format(position_prefix, position_counter)
             list_images_to_combine[tag_name] = {'list_of_files': _list_files,
                                                 'dict_metadata': _previous_metadata.copy(),
                                                 }
             master_list_images_to_combine[_previous_run] = list_images_to_combine
-            progress_bar.value = _index+1
 
         create_list_progress.close()
         del create_list_progress
@@ -251,7 +252,7 @@ class SequentialCombineImagesUsingMetadata(object):
         self.metadata_recap_textarea = box3.children[1]
         self.metadata_recap_textarea.value = str_metadata
 
-        hori_box = widgets.HBox([box1, box2, box3],
+        hori_box = widgets.HBox([box0, box1, box2, box3],
                                layout=widgets.Layout(width='100%'))
 
         self.list_of_positions_ui.on_trait_change(self.recap_positions_changed, name='value')
@@ -271,7 +272,7 @@ class SequentialCombineImagesUsingMetadata(object):
         _dict = self._get_dict_from_selected_run_position()
         return _dict['dict_metadata']
 
-    def _get_dict_for_selected_run_position(self):
+    def _get_dict_from_selected_run_position(self):
         selected_run = self.list_of_runs_ui.value
         selected_position = self.list_of_positions_ui.value
         return self.master_list_images_to_combine[selected_run][selected_position]
@@ -290,7 +291,7 @@ class SequentialCombineImagesUsingMetadata(object):
         list_files_of_files = self.master_list_images_to_combine[run_selected][position_selected]['list_of_files']
         self.list_of_files_ui.options = list_files_of_files
 
-        str_metadata = self.get_str_metadata(position_key=position_selected)
+        str_metadata = self.get_str_metadata(metadata_dict=self.get_metadata_for_selected_run_position())
         self.metadata_recap_textarea.value = str_metadata
 
     def recap_runs_changed(self):
@@ -309,10 +310,11 @@ class SequentialCombineImagesUsingMetadata(object):
 
         return result
 
-    def select_output_folder(self):
+    def select_output_folder_and_merge(self):
         self.output_folder_widget = ipywe.fileselector.FileSelectorPanel(instruction='select where to create the ' + \
                                                                                      'combined image ...',
                                                                          start_dir=self.working_dir,
+                                                                         next=self.merge,
                                                                          type='directory')
 
         self.output_folder_widget.show()
@@ -326,97 +328,99 @@ class SequentialCombineImagesUsingMetadata(object):
         else:
             return _algo
 
-    def define_output_filename(self):
-        list_files = self.files_list_widget.selected
-        short_list_files = [os.path.basename(_file) for _file in list_files]
-
-        merging_algo = self.__get_formated_merging_algo_name()
-        [default_new_name, ext] = self.__create_merged_file_name(list_files_names=short_list_files)
-
-        top_label = widgets.Label("You have the option to change the default output file name")
-
-        box = widgets.HBox([widgets.Label("Default File Name",
-                                          layout=widgets.Layout(width='20%')),
-                            widgets.Text(default_new_name,
-                                         layout=widgets.Layout(width='60%')),
-                            widgets.Label("_{}{}".format(merging_algo, ext),
-                                          layout=widgets.Layout(width='20%')),
-                            ])
-        self.default_filename_ui = box.children[1]
-        self.ext_ui = box.children[2]
-        vertical_box = widgets.VBox([top_label, box])
-        display(vertical_box)
-
-    def merging(self):
-        """combine images using algorithm provided"""
-
-        list_files = self.files_list_widget.selected
-        nbr_files = len(list_files)
-
-        # get merging algorithm
+    def get_merging_algorithm(self):
+        """return the algorithm function selected"""
         merging_algo = self.combine_method.value
-        algorithm = self.__add
-        if merging_algo =='arithmetic mean':
-            algorithm = self.__arithmetic_mean
+        # algorithm = self.__add
+        algorithm = SequentialCombineImagesUsingMetadata.__add
+        if merging_algo == 'arithmetic mean':
+            # algorithm = self.__arithmetic_mean
+            algorithm = SequentialCombineImagesUsingMetadata.__arithmetic_mean
         elif merging_algo == 'geometric mean':
-            algorithm = self.__geo_mean
+            # algorithm = self.__geo_mean
+            algorithm = SequentialCombineImagesUsingMetadata.__geo_mean
+        return algorithm
 
-        # get output folder
-        output_folder = os.path.abspath(self.output_folder_widget.selected)
-
-        o_load = Normalization()
-        o_load.load(file=list_files, notebook=True)
-        _data = o_load.data['sample']['data']
-
-        merging_ui = widgets.HBox([widgets.Label("Merging Progress",
-                                                 layout=widgets.Layout(width='20%')),
-                                   widgets.IntProgress(max=2)])
-        display(merging_ui)
-        w1 = merging_ui.children[1]
-
-        combined_data = self.__merging_algorithm(algorithm, _data)
-        w1.value = 1
-
-        #_new_name = self.__create_merged_file_name(list_files_names=o_load.data['sample']['file_name'])
-        _new_name = self.default_filename_ui.value + self.ext_ui.value
-        output_file_name = os.path.join(output_folder, _new_name)
-
-        file_handler.save_data(data=combined_data, filename=output_file_name)
-
-        w1.value = 2
-
-        display(HTML('<span style="font-size: 20px; color:blue">File created: ' + \
-                     os.path.basename(output_file_name) + '</span>'))
-        display(HTML('<span style="font-size: 20px; color:blue">In Folder: ' + \
-                     output_folder + '</span>'))
-
-    def __create_merged_file_name(self, list_files_names=[]):
-        """Create the new base name using a combine name of all the input file
-
-        :param list_files_names: ['/Users/j35/image001.fits','/Users/j35/iamge002.fits']
-        :return:
-            'image001_image002.fits'
-        """
-        ext = ''
-        list_base_name = []
-        for _file in list_files_names:
-            basename = os.path.basename(_file)
-            [_name, ext] = os.path.splitext(basename)
-            list_base_name.append(_name)
-
-        return ('_'.join(list_base_name), ext)
-
+    @staticmethod
     def __add(self, data_array):
         return np.sum(data_array, axis=0)
 
+    @staticmethod
     def __arithmetic_mean(self, data_array):
         return np.mean(data_array, axis=0)
 
+    @staticmethod
     def __geo_mean(self, data_array):
         return gmean(data_array, axis=0)
 
+    @staticmethod
     def __merging_algorithm(self, function_, *args):
         return function_(*args)
+
+    def merge(self, output_folder):
+        """combine images using algorithm provided"""
+
+        merging_list = self.master_list_images_to_combine
+        algorithm = self.get_merging_algorithm()
+
+        merging_ui = widgets.HBox([widgets.Label("Merging Progress",
+                                                 layout=widgets.Layout(width='20%')),
+                                   widgets.IntProgress(max=len(merging_list.keys()),
+                                                       layout=widgets.Layout(width='80%'))])
+        display(merging_ui)
+        progress_bar_ui = merging_ui.children[1]
+
+        _run_index = 0
+        for _run in merging_list.keys():
+
+            positions_dict = merging_list[_run]
+
+            for _position in positions_dict.keys():
+
+                list_of_files = positions_dict[_position]
+
+                o_load = Normalization()
+                o_load.load(file=list_of_files, notebook=True)
+                _data = o_load.data['sample']['data']
+
+                combined_data = self.__merging_algorithm(algorithm, _data)
+
+                _new_name = self._define_merged_file_name(output_folder=output_folder,
+                                                          run_label=_run,
+                                                          position_label=_position)
+                output_file_name = os.path.join(output_folder, _new_name)
+
+                file_handler.save_data(data=combined_data, filename=output_file_name)
+
+            progress_bar_ui.value = _run_index + 1
+
+        progress_bar_ui.close()
+        del progress_bar_ui
+
+        # display(HTML('<span style="font-size: 20px; color:blue">File created: ' + \
+        #              os.path.basename(output_file_name) + '</span>'))
+        # display(HTML('<span style="font-size: 20px; color:blue">In Folder: ' + \
+        #              output_folder + '</span>'))
+
+    def _define_merged_file_name(self, output_folder='', run_label='', position_label=''):
+        """Create the new merged file name using the run, position labels
+
+            ex: run_label = "run1"
+                position_label = "position6"
+                output_folder = "/Users/zizou/"
+                data was taken from folder "exp28"
+
+                return: "/Users/zizou/exp28_add_merged/run1_position6.tiff"
+        """
+        folder_selected = self.folder_selected
+        algorithm_selected = self.__get_formated_merging_algo_name()
+
+        output_folder = os.path.join(output_folder, "{}_{}".format(folder_selected, algorithm_selected))
+        return os.path.join(output_folder, "{}_{}.tiff".format(run_label, position_label))
+
+
+
+
 
 
 
