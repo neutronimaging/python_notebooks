e31ae71a7e991f53c4dca56f263d6c7e2680c5e9
JeanBilheux <bilheuxjm@ornl.gov>
2020-06-02 08:31:18 -0400

    Renamed notebook and file. Added comboboxes that show the list of keys in DASlogs

---
+++ __code/extract_nexus_daslogs/extract.py
@@ -0,0 +1,22 @@
+from __code.file_folder_browser import FileFolderBrowser
+
+
+class Extract(FileFolderBrowser):
+
+	def __init__(self, working_dir=''):
+		super(Extract, self).__init__(working_dir=working_dir)
+
+	def select_nexus(self):
+		o_file = FileFolderBrowser(working_dir=self.working_dir,
+		                           next_function=self.select_metadata)
+		o_file.select_images(instruction="Select the NeXus files to extract infos from!",
+		                     multiple_flag=True,
+		                     filters={"NeXus": '*.nxs.h5'})
+
+	def select_metadata(self, list_nexus=None):
+		if list_nexus is None:
+			return
+
+		first_nexus_selected = list_nexus[0]
+	
+
