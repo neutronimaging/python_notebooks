56df4b9535fb27f8f85adedfbe7f6ea5a1bccaa1
JeanBilheux <bilheuxjm@ornl.gov>
2020-06-03 08:38:31 -0400

    Added code that retrieve a given info from HDF5 and implemented unit test at the same time

--- __code/extract_nexus_daslogs/extract.py
+++ __code/extract_nexus_daslogs/extract.py
@@ -1,11 +1,15 @@
 import h5py
 from pathlib import Path
 from ipywidgets import widgets
-
+from IPython.core.display import display
+from __code.nexus_handler import get_list_entries
 from __code.file_folder_browser import FileFolderBrowser
 
 
 class Extract(FileFolderBrowser):
+
+	first_nexus_selected = ''
+	widget_height = "400px"
 
 	def __init__(self, working_dir=''):
 		super(Extract, self).__init__(working_dir=working_dir)
@@ -21,12 +25,115 @@ class Extract(FileFolderBrowser):
 		if list_nexus is None:
 			return
 
-		first_nexus_selected = list_nexus[0]
+		self.first_nexus_selected = list_nexus[0]
+		self.dict_daslogs_keys = get_list_entries(nexus_file_name=self.first_nexus_selected,
+		                             starting_entries=['entry','DASlogs'])
 
-		dict_daslogs_keys = {}
-		with h5py.File(first_nexus_selected, 'r') as nxs:
-			for key in nxs['entry']['DASlogs'].keys():
-				dict_daslogs_keys[key] = list(nxs['entry']['DASlogs'][key].keys())
+		self.display_widgets()
 
+	def display_widgets(self):
 
+		dict_daslogs_keys = self.dict_daslogs_keys
+		nexus_path = self.first_nexus_selected
+
+		self.list_daslogs_keys = list(dict_daslogs_keys.keys())
+		first_daslogs_key = self.list_daslogs_keys[0]
+		list_keys_of_first_daslogs_key = list(dict_daslogs_keys[first_daslogs_key])
+		first_key = list_keys_of_first_daslogs_key[0]
+		value_of_first_selected_element = None
+		with h5py.File(nexus_path, 'r') as nxs:
+			value_of_first_selected_element = (list(nxs['entry']['DASlogs'][first_daslogs_key][first_key]))
+
+		search_box = widgets.HBox([widgets.Label("Search:"),
+		                           widgets.Text("",
+		                                        layout=widgets.Layout(width="30%"))])
+		search_text_widget = search_box.children[1]
+		search_text_widget.observe(self.search_text_changed, names='value')
+
+		key_layout = widgets.VBox([widgets.Label("X-axis"),
+		                           widgets.Select(options=list_keys_of_first_daslogs_key),
+		                           widgets.Label("Y-axis"),
+		                           widgets.Select(options=list_keys_of_first_daslogs_key),
+		                           ])
+		value_layout = widgets.VBox([widgets.Label("Value"),
+		                             widgets.Select(options=value_of_first_selected_element),
+		                             widgets.Label("Value"),
+		                             widgets.Select(options=value_of_first_selected_element)],
+		                            )
+
+		self.use_absolute_time_offset = widgets.Checkbox(value=True,
+		                                                 description='Use absolute time.')
+
+		hori_box = widgets.HBox([widgets.Select(options=self.list_daslogs_keys,
+		                                        value=first_daslogs_key,
+		                                        layout=widgets.Layout(width="400px",
+		                                                              height=self.widget_height)),
+		                         key_layout,
+		                         value_layout],
+		                        layout=widgets.Layout())
+
+		display(search_box)
+		display(hori_box)
+		display(self.use_absolute_time_offset)
+
+		self.top_keys_widgets = hori_box.children[0]
+
+		self.x_axis_intermediate_key_widget = key_layout.children[1]
+		self.y_axis_intermediate_key_widget = key_layout.children[3]
+
+		self.x_axis_intermediate_value_widget = value_layout.children[1]
+		self.y_axis_intermediate_value_widget = value_layout.children[3]
+
+		self.top_keys_widgets.observe(self.top_keys_changed, names='value')
+		self.x_axis_intermediate_key_widget.observe(self.x_axis_intermediate_value_changed, names='value')
+		self.y_axis_intermediate_key_widget.observe(self.y_axis_intermediate_value_changed, names='value')
+
+		self.top_keys_widget_value = self.top_keys_widgets.value
+
+	def top_keys_changed(self, value):
+		new_top_key = value['new']
+		self.top_keys_widget_value = new_top_key
+		list_intermediate_keys = list(self.dict_daslogs_keys[new_top_key])
+		self.x_axis_intermediate_key_widget.options = list_intermediate_keys
+		self.x_axis_intermediate_key_widget.value = list_intermediate_keys[0]
+		self.y_axis_intermediate_key_widget.options = list_intermediate_keys
+		self.y_axis_intermediate_key_widget.value = list_intermediate_keys[0]
+
+	def x_axis_intermediate_value_changed(self, value=None):
+		new_intermediate_key = value['new']
+		first_daslogs_key =self.top_keys_widget_value
+		with h5py.File(self.first_nexus_selected, 'r') as nxs:
+			value_of_first_selected_element = (list(nxs['entry']['DASlogs'][first_daslogs_key][new_intermediate_key]))
+		self.x_axis_intermediate_value_widget.options = value_of_first_selected_element
+
+	def y_axis_intermediate_value_changed(self, value=None):
+		new_intermediate_key = value['new']
+		# value_of_first_selected_element = None
+		first_daslogs_key = self.top_keys_widget_value
+		with h5py.File(self.first_nexus_selected, 'r') as nxs:
+			value_of_first_selected_element = (list(nxs['entry']['DASlogs'][first_daslogs_key][new_intermediate_key]))
+		self.y_axis_intermediate_value_widget.options = value_of_first_selected_element
+
+	def search_text_changed(self, value):
+		new_text = value['new']
+		if new_text == "":
+			self.top_keys_widgets.value = self.list_daslogs_keys[0]
+			return
+
+		for key in self.list_daslogs_keys:
+			if new_text in key:
+				self.top_keys_widgets.value = key
+				return
+
+	def extract(self):
+		top_key_widget_value = self.top_keys_widget_value
+		x_axis_key = self.x_axis_intermediate_key_widget.value
+		y_axis_key = self.y_axis_intermediate_key_widget.value
+		use_absolute_time_offset = True if self.use_absolute_time_offset.value \
+		                                   and (x_axis_key == 'time' or y_axis_key == 'time') else False
+
+		print(f"top_key_widget_value: {top_key_widget_value}")
+		print(f"x_axis_key: {x_axis_key}")
+		print(f"y_axis_key: {y_axis_key}")
+		print(use_absolute_time_offset)
 
--- __code/nexus_handler.py
+++ __code/nexus_handler.py
@@ -8,13 +8,15 @@ def get_list_entries(nexus_file_name=None, starting_entries=None):
 	:param nexus_file_name: hdf5 full file path
 	:param starting_entries: list of entries
 		example: starting_entries = ['entry','DASlogs']
-	:return:
+	:return: dictionary showing the structure of the hdf5 from the starting_entries level
+	    example: {'PV1': ['value', 'maximum_value', 'minimum_value', 'mean'],
+	              'PV2': ['value', 'maximum_value', 'minimum_value', 'mean']}
 	'''
 	if nexus_file_name is None:
-		raise ValueError("Please provide a full path to a nexus file name")
+		raise ValueError("Please provide a full path to a nexus file name!")
 
 	if not Path(nexus_file_name).exists():
-		raise ValueError("File do not exist")
+		raise ValueError("File do not exist!")
 
 	dict_daslogs_keys = {}
 	with h5py.File(nexus_file_name, 'r') as nxs:
@@ -30,3 +32,29 @@ def get_list_entries(nexus_file_name=None, starting_entries=None):
 			dict_daslogs_keys[key] = list(nxs_path.get(key).keys())
 
 	return dict_daslogs_keys
+
+
+def get_entry_value(nexus_file_name=None, entry_path=None):
+	'''
+
+	:param nexus_file_name: hdf5 full file path
+	:param entry_path: full path through the file to retrieve the information
+		example: ['entry', 'DASlogs', 'PV1', 'value']
+	:return: value at the given path in the hdf5 file
+	'''
+	if nexus_file_name is None:
+		raise ValueError("Please provide a full path to a nexus file name!")
+
+	if not Path(nexus_file_name).exists():
+		raise ValueError("File do not exist!")
+
+	with h5py.File(nexus_file_name, 'r') as nxs:
+
+		try:
+			nxs_path = nxs
+			for _item in entry_path:
+				nxs_path = nxs_path.get(_item)
+		except AttributeError:
+			raise AttributeError("Path specify in the HDF5 is wrong!")
+
+		return list(nxs_path)
