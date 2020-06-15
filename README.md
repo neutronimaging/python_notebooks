dcab127f890dc8f337369992cfb9d5bdff13fec5
JeanBilheux <bilheuxjm@ornl.gov>
2020-06-15 12:25:55 -0400

    Updated script to prepare notebooks

--- README.md
+++ README.md
@@ -14,3 +14,11 @@ and before pushing to repository
 ```  
 $ python before_and_after_github_script.py -a
 ```
+
+This will reset all the notebooks (clear output)
+
+To turn debugging mode on, add the flag -d (--use_debugging_mode) to the command
+
+```
+$ python before_and_after_github_script.py -a -d
+```
--- before_and_after_github_script.py
+++ before_and_after_github_script.py
@@ -2,12 +2,15 @@ import argparse
 import os
 import glob
 
+from __code.file_handler import read_ascii, make_ascii_file_from_string
+
 # this script will convert the notebooks to .py before pushing to github and the .py files back to
 # notebooks after pulling from github
 
 parser = argparse.ArgumentParser(description='Before Push or After Pull from GitHub')
 parser.add_argument('-b', '--before_push', action="store_true", help='Convert all .ipynb to .py in converted_notebooks folder')
 parser.add_argument('-a', '--after_pull', action="store_true", help='Convert all .py from converted_notebooks folder to .ipynb')
+parser.add_argument('-d', '--use_debugging_mode', action="store_true", help='Change the debugging mode on/off')
 
 def run():
 
@@ -37,6 +40,21 @@ def run():
 
     elif args.after_pull:
         os.system('jupytext --to ipynb converted_notebooks/*.py')
+
+    # turn debugging flag ON/OFF
+    print("---> debugging mode is {}".format(args.use_debugging_mode))
+    if args.use_debugging_mode:
+        print("Turning Debugging Mode ON!")
+    else:
+        print("Turning Debugging Mode OFF!")
+
+    config_file = os.path.join(_top_path, '__code/config.py')
+    _config_file_contain = read_ascii(config_file)
+    _parse = _config_file_contain.split('\n')
+    _parse[0] = 'debugging = {}'.format(args.use_debugging_mode)
+    _new_contain = '\n'.join(_parse)
+
+    make_ascii_file_from_string(text=_new_contain, filename=config_file)
 
 
 if __name__ == '__main__':
