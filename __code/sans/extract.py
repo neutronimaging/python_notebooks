a48874a5ce2a2ce74e38429c2b882aa4a48f7b69
JeanBilheux <bilheuxjm@ornl.gov>
2020-06-18 16:07:45 -0400

    working on reformating the output following Lisa's request

--- __code/sans/extract.py
+++ __code/sans/extract.py
@@ -9,7 +9,7 @@ from collections import OrderedDict
 import numpy as np
 
 from __code import fileselector
-from __code.nexus_handler import get_list_entries, get_entry_value
+from __code.nexus_handler import get_list_entries
 from __code.file_folder_browser import FileFolderBrowser
 from __code.time_utility import AbsoluteTimeHandler, RelativeTimeHandler
 from __code.interpolation_utilities import Interpolation
@@ -26,7 +26,7 @@ class Initializer:
 	def select_instrument(self):
 		list_instruments = list(LIST_SANS_INSTRUMENTS.keys())
 		instrument_ui = widgets.HBox([widgets.Label("Select your instrument",
-			                                        layout=widgets.Layout(width='15%')),
+		                                            layout=widgets.Layout(width='15%')),
 		                              widgets.Select(options=list_instruments,
 		                                             layout=widgets.Layout(width='30%',
 		                                                                   height='50px'))])
@@ -44,7 +44,6 @@ class Initializer:
 
 
 class Extract(FileFolderBrowser):
-
 	first_nexus_selected = ''
 	widget_height = "400px"
 	full_list_selected = {}
@@ -59,17 +58,11 @@ class Extract(FileFolderBrowser):
 			self.list_parameters = biosans_parameters
 
 		self.init_full_list_selected()
+		self.list_nexus = None
 
 	def init_full_list_selected(self):
 		for para in self.list_parameters.keys():
 			self.full_list_selected[para] = []
-
-	@staticmethod
-	def get_short_version_instrument(instrument):
-		if 'CG2' in instrument:
-			return 'CG2'
-		else:
-			return 'CG3'
 
 	def select_reductionlog(self):
 		self.o_file = fileselector.FileSelectorPanelWithJumpFolders(start_dir=self.working_dir,
@@ -98,12 +91,12 @@ class Extract(FileFolderBrowser):
 
 		# list of keys
 		hori_box = widgets.HBox([widgets.Select(options=self.list_keys,
-	                                            layout=widgets.Layout(width="400px",
-	                                                                  height=self.widget_height)),
-		                         widgets.SelectMultiple(options=self.list_values,
 		                                        layout=widgets.Layout(width="400px",
-		                                                              height=self.widget_height))],
-	                             )
+		                                                              height=self.widget_height)),
+		                         widgets.SelectMultiple(options=self.list_values,
+		                                                layout=widgets.Layout(width="400px",
+		                                                                      height=self.widget_height))],
+		                        )
 		display(hori_box)
 		[self.left_widget_ui, self.right_widget_ui] = hori_box.children
 		self.left_widget_ui.observe(self.left_widget_changed, names='value')
@@ -136,15 +129,16 @@ class Extract(FileFolderBrowser):
 
 	def search_text_changed(self, value):
 		pass
-		# new_text = value['new']
-		# if new_text == "":
-		# 	self.top_keys_widgets.value = self.list_daslogs_keys[0]
-		# 	return
-		#
-		# for key in self.list_daslogs_keys:
-		# 	if new_text in key:
-		# 		self.top_keys_widgets.value = key
-		# 		return
+
+	# new_text = value['new']
+	# if new_text == "":
+	# 	self.top_keys_widgets.value = self.list_daslogs_keys[0]
+	# 	return
+	#
+	# for key in self.list_daslogs_keys:
+	# 	if new_text in key:
+	# 		self.top_keys_widgets.value = key
+	# 		return
 
 	def select_metadata(self, list_nexus=None):
 		if list_nexus is None:
@@ -158,63 +152,105 @@ class Extract(FileFolderBrowser):
 
 		self.display_widgets()
 
+	def export(self):
+		self.output_folder_ui = fileselector.FileSelectorPanelWithJumpFolders(
+				instruction='select where to create the ' + \
+				            'ascii file',
+				start_dir=self.working_dir,
+				next=self.extract_all_in_one,
+				type='directory',
+				newdir_toolbar_button=True,
+				show_jump_to_share=False)
+
+	@staticmethod
+	def reformat_dict(full_dict):
+		'''
+		we need to go from {'nexus1': {'path1': value1', 'path2': value2},
+		 ...}
+		to
+		{'path1': {'nexus1': value1, 'nexus2': value2},
+		 ...}
+		'''
+		new_full_dict = OrderedDict()
+		for _nexus_key in full_dict.keys():
+			for _path_key in full_dict[_nexus_key].keys():
+				_value = full_dict[_nexus_key][_path_key]
+				if not (_path_key in new_full_dict.keys()):
+					new_full_dict[_path_key] = OrderedDict()
+				new_full_dict[_path_key][_nexus_key] = _value
+
+		return new_full_dict
+
+
+	def extract_all_in_one(self, output_folder):
+		self.output_folder_ui.shortcut_buttons.close()
+		output_file_name = Extract.create_output_file_name(output_folder=output_folder,
+		                                                nbr_nexus=len(self.list_nexus))
+		full_list_selected = self.full_list_selected
+
+		# get list of path
+		list_entry_path = []
+		for top_key in full_list_selected.keys():
+			top_path = copy.deepcopy(self.list_parameters[top_key]['path'])
+			for inside_key in full_list_selected[top_key]:
+				full_path = copy.deepcopy(top_path)
+				full_path.append(inside_key)
+				# metadata_tag = "# {} -> {}".format(top_key, inside_key)
+				list_entry_path.append(full_path)
+
+		list_nexus = self.list_nexus
+		full_dict = OrderedDict()
+		metadata = []
+		for _index, _nexus in enumerate(list_nexus):
+			value = Extract.get_entry_value(nexus_file_name=_nexus,
+			                                list_entry_path=list_entry_path)
+			full_dict[_nexus] = value
+			metadata.append("# column #{}: {}".format(_index, _nexus))
+
+		full_dict_with_path_as_key = Extract.reformat_dict(full_dict)
+
+		metadata.append("#")
+		for nexus_key in full_dict.keys():
+
+			_nexus_entry = full_dict[nexus_key]
+			for _path in _nexus_entry.keys():
+				pass
+
+
+
+
+
+
+
+
+
+
+		# metadata = ['# nexus file name: ' + nexus_file_name]
+		#
+		# for top_key in full_list_selected.keys():
+		# 	top_path = self.list_parameters[top_key]['path']
+		# 	for internal_key in full_list_selected[top_key]:
+		# 		entry_path = copy.deepcopy(top_path)
+		# 		entry_path.append(internal_key)
+		#
+		# 		value = self.get_entry_value(nexus_file_name=nexus_file_name,
+		# 		                             entry_path=entry_path)
+		#
+		# 		metadata.append('# {} -> {}: {}'.format(top_key, internal_key, value))
+
 	def extract_all(self, output_folder):
 		self.output_folder_ui.shortcut_buttons.close()
 
 		list_nexus = self.list_nexus
 		for _nexus in list_nexus:
 			self.extract(nexus_file_name=_nexus,
-			             output_folder=output_folder)
+						 output_folder=output_folder)
 
 	def makeup_output_file_name(self, nexus_file_name='', output_folder='./'):
 		short_nexus_file_name = os.path.basename(nexus_file_name)
 		base_name, ext = os.path.splitext(short_nexus_file_name)
 		new_name = str(Path(output_folder) / "{}_extracted.txt".format(base_name))
 		return new_name
-
-	def get_entry_value(self, nexus_file_name=None, entry_path=None):
-		'''
-
-		:param nexus_file_name: hdf5 full file path
-		:param entry_path: full path through the file to retrieve the information
-			example: ['entry', 'DASlogs', 'PV1', 'value']
-		:return: value at the given path in the hdf5 file
-		'''
-		if nexus_file_name is None:
-			raise ValueError("Please provide a full path to a nexus file name!")
-
-		if not Path(nexus_file_name).exists():
-			raise ValueError("File do not exist!")
-
-		with h5py.File(nexus_file_name, 'r') as nxs:
-
-			try:
-				nxs_path = nxs
-				for _item in entry_path:
-					nxs_path = nxs_path.get(_item)
-
-			except AttributeError:
-				raise AttributeError("Path specify in the HDF5 is wrong!")
-
-			if nxs_path is None:
-				return None
-
-			if type(nxs_path) == h5py._hl.group.Group:
-				result = {}
-				for _key in nxs_path.keys():
-					value = nxs_path.get(_key)
-					if type(value) == h5py._hl.dataset.Dataset:
-						result[_key] = value[()]
-					else:
-						result[_key] = value
-
-				return result
-
-			if type(nxs_path) == h5py._hl.dataset.Dataset:
-				return nxs_path[()]
-
-			return None
-
 
 	def extract(self, nexus_file_name='', output_folder='./'):
 
@@ -229,7 +265,7 @@ class Extract(FileFolderBrowser):
 				entry_path.append(internal_key)
 
 				value = self.get_entry_value(nexus_file_name=nexus_file_name,
-			                            entry_path=entry_path)
+				                             entry_path=entry_path)
 
 				metadata.append('# {} -> {}: {}'.format(top_key, internal_key, value))
 
@@ -237,16 +273,6 @@ class Extract(FileFolderBrowser):
 		                                                output_folder=output_folder)
 		make_ascii_file(metadata=metadata,
 		                output_file_name=output_file_name)
-
-	def export(self):
-		self.output_folder_ui = fileselector.FileSelectorPanelWithJumpFolders(
-				instruction='select where to create the ' + \
-				            'ascii file',
-				start_dir=self.working_dir,
-				next=self.extract_all,
-				type='directory',
-				newdir_toolbar_button=True,
-				show_jump_to_share=False)
 
 	@staticmethod
 	def create_output_file(file_name=None, dictionary=None):
@@ -281,3 +307,67 @@ class Extract(FileFolderBrowser):
 
 		display(HTML('<span style="font-size: 20px; color:blue">The following ASCII file has been created: ' +
 		             '</span><span style="font-size: 20px; color:green">' + file_name + '</span>'))
+
+	@staticmethod
+	def create_output_file_name(output_folder='./', nbr_nexus=0):
+		clean_output_folder = os.path.abspath(output_folder)
+		short_file_name = "metadata_from_{}_nexus.txt".format(nbr_nexus)
+		return str(Path(clean_output_folder) / short_file_name)
+
+	@staticmethod
+	def get_short_version_instrument(instrument):
+		if 'CG2' in instrument:
+			return 'CG2'
+		else:
+			return 'CG3'
+
+	@staticmethod
+	def get_entry_value(nexus_file_name=None, list_entry_path=None):
+		'''
+
+		:param nexus_file_name: hdf5 full file path
+		:param entry_path: full path through the file to retrieve the information
+			example: ['entry', 'DASlogs', 'PV1', 'value']
+		:return: value at the given path in the hdf5 file
+		'''
+		if nexus_file_name is None:
+			raise ValueError("Please provide a full path to a nexus file name!")
+
+		if not Path(nexus_file_name).exists():
+			raise ValueError("File do not exist!")
+
+		result_dict = OrderedDict()
+		with h5py.File(nexus_file_name, 'r') as nxs:
+
+			for _entry_path in list_entry_path:
+
+				id = "_".join(_entry_path)
+
+				nxs_path = None
+				try:
+					nxs_path = nxs
+					for _item in _entry_path:
+						nxs_path = nxs_path.get(_item)
+
+				except AttributeError:
+					result_dict[id] = None
+
+				# format output
+				if nxs_path is None:
+					result_dict[id] = nxs_path
+
+				elif type(nxs_path) == h5py._hl.group.Group:
+					result = {}
+					for _key in nxs_path.keys():
+						value = nxs_path.get(_key)
+						if type(value) == h5py._hl.dataset.Dataset:
+							result[_key] = np.squeeze(value[()])
+						else:
+							result[_key] = value
+
+					result_dict[id] = result
+
+				elif type(nxs_path) == h5py._hl.dataset.Dataset:
+					result_dict[id] = np.array(nxs_path[()])
+
+		return result_dict
