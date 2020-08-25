d3a19b41208dfe95fac78242ba2b88b9dc5cf5dc
JeanBilheux <bilheuxjm@ornl.gov>
2020-08-25 07:28:30 -0400

    movign rows around works

--- __code/bragg_edge/bragg_edge_peak_fitting_gui_utility.py
+++ __code/bragg_edge/bragg_edge_peak_fitting_gui_utility.py
@@ -113,6 +113,8 @@ class GuiUtility:
 
 	def fill_march_dollase_table(self, list_state=None, list_initial_parameters=None):
 		table_ui = self.parent.ui.march_dollase_user_input_table
+		o_table = TableHandler(table_ui=table_ui)
+		o_table.remove_all_rows()
 
 		march_dollase_row_height = {0: 110,
 		                            'other': 60}
--- __code/bragg_edge/march_dollase.py
+++ __code/bragg_edge/march_dollase.py
@@ -1,4 +1,5 @@
 from __code.table_handler import TableHandler
+from __code.bragg_edge.bragg_edge_peak_fitting_gui_utility import GuiUtility
 
 
 class MarchDollase:
@@ -27,9 +28,29 @@ class MarchDollase:
 		return row_selected
 
 	def move_row_up(self):
-		row_selected = self.get_row_selected()
-		print(f"row_selected {row_selected}")
+		self.move_table_row(to_row_offset=-1)
 
 	def move_row_down(self):
+		self.move_table_row(to_row_offset=+1)
+
+	def move_table_row(self, to_row_offset=-1):
+		"""
+		:param to_row_offset: +1 means moving row to the next row, -1 means moving up by one row
+		"""
 		row_selected = self.get_row_selected()
-		print(f"row_selected {row_selected}")
+		march_dollase_fitting_history_table = self.parent.march_dollase_fitting_history_table
+		row_to_move = march_dollase_fitting_history_table.pop(row_selected)
+
+		new_row = row_selected + to_row_offset
+
+		march_dollase_fitting_history_table.insert(new_row, row_to_move)
+		self.parent.march_dollase_fitting_history_table = march_dollase_fitting_history_table
+
+		o_gui = GuiUtility(parent=self.parent)
+		o_gui.fill_march_dollase_table(list_state=self.parent.march_dollase_fitting_history_table,
+		                               list_initial_parameters=self.parent.march_dollase_fitting_initial_parameters)
+
+		# keep current selection on new row
+		o_table = TableHandler(table_ui=self.parent.march_dollase_user_input_table)
+		o_table.select_row(row=new_row)
+		self.table_clicked(row=new_row)
