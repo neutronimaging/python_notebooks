e16248f8819a64eb43ead4434fcb4b63bf7d5eff
JeanBilheux <bilheuxjm@ornl.gov>
2020-09-03 09:20:51 -0400

    added a unit test for 4 arrays

--- tests/test_combine_images_with_outliners.py
+++ tests/test_combine_images_with_outliners.py
@@ -26,12 +26,26 @@ class TestCombineImagesWithOutliners:
 		with pytest.raises(ValueError):
 			CombineImages.check_arrays_have_same_size_and_dimensions(list_array=list_array)
 
-	def test_mean_without_outliers(self):
+	def test_mean_without_outliers_3_arrays(self):
 		array1 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 		array2 = np.array([[5, 5, 5], [5, 5, 5], [5, 5, 5]])
 		array3 = np.array([[10, 2, 5], [10, 10, 10], [3, 3, 3]])
 		mean_array_calculated = CombineImages.mean_without_outliers(list_array=[array1, array2, array3])
 		mean_array_expected = np.array([[5, 2, 5], [5, 5, 6], [5, 5, 5]])
+		[nbr_row, nbr_col] = np.shape(mean_array_expected)
+		for _row in np.arange(nbr_row):
+			for _col in np.arange(nbr_col):
+				_counts_expected = mean_array_expected[_row, _col]
+				_count_calculated = mean_array_calculated[_row, _col]
+				assert _counts_expected == _count_calculated
+
+	def test_mean_without_outliers_4_arrays(self):
+		array1 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+		array2 = np.array([[5, 5, 5], [5, 5, 5], [5, 5, 5]])
+		array3 = np.array([[20, 0, 0], [6, 6, 1], [1, 1, 20]])
+		array4 = np.array([[10, 2, 5], [10, 10, 10], [3, 3, 3]])
+		mean_array_calculated = CombineImages.mean_without_outliers(list_array=[array1, array2, array3, array4])
+		mean_array_expected = np.array([[7.5, 2, 4], [5.5, 5.5, 5.5], [4, 4, 7]])
 		[nbr_row, nbr_col] = np.shape(mean_array_expected)
 		for _row in np.arange(nbr_row):
 			for _col in np.arange(nbr_col):
