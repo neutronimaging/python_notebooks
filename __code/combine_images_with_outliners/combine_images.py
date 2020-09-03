7ad3f632e1b4d1669712abb164aa75ad104b3480
JeanBilheux <bilheuxjm@ornl.gov>
2020-09-03 09:17:24 -0400

    mean without outliers works

--- __code/combine_images_with_outliners/combine_images.py
+++ __code/combine_images_with_outliners/combine_images.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class CombineImages:
 
 	def __init__(self, list_array=None):
@@ -8,10 +11,30 @@ class CombineImages:
 		if len(list_array) < 3:
 			raise ValueError("Please provide at least 3 arrays!")
 
-		self.check_arrays_have_same_size(list_array=list_array)
+		self.check_arrays_have_same_size_and_dimensions(list_array=list_array)
 
 	@staticmethod
-	def check_arrays_have_same_size(list_array=None):
+	def check_arrays_have_same_size_and_dimensions(list_array=None):
 		list_len_array = set([len(_array) for _array in list_array])
 		if len(list_len_array) > 1:
 			raise ValueError("Arrays do not have the same size!")
+
+		# make sure all the dimensions have the same size
+		list_shape_array = set([_array.shape for _array in list_array])
+		if len(list_shape_array) > 1:
+			raise ValueError("Arrays do not have the same dimension!")
+
+	@staticmethod
+	def mean_without_outliers(list_array=None):
+		global_array = []
+		for _array in list_array:
+			global_array.append(_array)
+		nbr_arrays = len(global_array)
+
+		min_array = np.min(global_array, 0)
+		max_array = np.max(global_array, 0)
+
+		sum_array = np.sum(global_array, 0) - max_array - min_array
+		mean_array = sum_array / (nbr_arrays - 2)
+
+		return mean_array
