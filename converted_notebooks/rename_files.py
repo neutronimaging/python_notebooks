890c501f23541131a7e5211152c3ffe9ffec0320
JeanBilheux <bilheuxjm@ornl.gov>
2020-09-04 14:25:45 -0400

    ready to be deployed and tested

--- converted_notebooks/rename_files.py
+++ converted_notebooks/rename_files.py
@@ -29,44 +29,17 @@ from __code.__all import custom_style
 custom_style.style()
 
 # + [markdown] run_control={"frozen": false, "read_only": false}
-# # Select Input Folder
-
-# + [markdown] run_control={"frozen": false, "read_only": false}
-# Select input folder that contains all the images you need to rename.
+# # Select Input Folder and define new naming schema
 
 # + run_control={"frozen": false, "read_only": false}
 o_format = FormatFileNameIndex(working_dir=system.System.get_working_dir())
 o_format.select_input_folder()
 
 # + [markdown] run_control={"frozen": false, "read_only": false}
-# # Define New Naming Schema
-
-# + run_control={"frozen": false, "read_only": false}
-# keep most dominant files extension
-o_format.calculate_most_dominant_files()
-o_format.get_most_dominant_files()
-
-o_schema = NamingSchemaDefinition(o_format=o_format)
-o_schema.show()
-
-# + [markdown] run_control={"frozen": false, "read_only": false}
 # # Select Output Folder
 
 # + run_control={"frozen": false, "read_only": false}
-o_schema.select_export_folder()
-
-# +
-
-
-
-
-
-
-
-
-
-
-
-
+o_format.o_schema.select_export_folder()
+# -
 
 
