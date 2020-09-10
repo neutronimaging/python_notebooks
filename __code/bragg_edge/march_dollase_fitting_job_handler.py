9bae4b131fcad38156b1013f0c740b32b2eaa3b9
JeanBilheux <bilheuxjm@ornl.gov>
2020-09-10 17:23:01 -0400

    working on running the march-dollase fitting now

--- __code/bragg_edge/march_dollase_fitting_job_handler.py
+++ __code/bragg_edge/march_dollase_fitting_job_handler.py
@@ -17,7 +17,7 @@ class MarchDollaseFittingJobHandler:
 			return False
 
 	def initialize_d_spacing(self):
-		d_spacing = self.get_initial_parameter_value(name_of_parameter='d_spacing')
+		d_spacing = self.get_d_spacing()
 		self.parent.march_dollase_fitting_initial_parameters['d_spacing'] = d_spacing
 
 	def initialize_fitting_input_dictionary(self):
@@ -34,9 +34,9 @@ class MarchDollaseFittingJobHandler:
 		[d_spacing_flag, sigma_flag, alpha_flag, a1_flag, a2_flag, a5_flag, a6_flag] = \
 			march_dollase_fitting_history_table[0]
 
-		d_spacing = self.get_initial_parameter_value(name_of_parameter='d_spacing')
-		sigma = self.get_initial_parameter_value(name_of_parameter='sigma')
-		alpha = self.get_initial_parameter_value(name_of_parameter='alpha')
+		d_spacing = self.get_d_spacing()
+		sigma = self.get_sigma()
+		alpha = self.get_alpha()
 
 		self.parent.march_dollase_fitting_initial_parameters['d_spacing'] = d_spacing
 		fitting_input_dictionary = self.parent.fitting_input_dictionary
@@ -85,9 +85,6 @@ class MarchDollaseFittingJobHandler:
 				a2 = self.get_a2(advanced_mode=self.is_advanced_mode())
 				fitting_input_dictionary['rois'][_row]['fitting']['march_dollase']['a2'] = a2
 
-		# import pprint
-		# pprint.pprint(fitting_input_dictionary['rois'][0]['fitting']['march_dollase'])
-
 	def isolate_left_center_right_axis(self, row=-1):
 		bragg_edge_range = self.parent.fitting_input_dictionary['bragg_edge_range']
 		[global_left_index, global_right_index] = [np.int(bragg_edge_range[0]),
@@ -109,29 +106,18 @@ class MarchDollaseFittingJobHandler:
 		        'right_part': {'lambda_x_axis': lambda_x_axis[right_index:],
 		                       'y_axis': y_axis[right_index:]}}
 
-	# def isolate_left_and_right_part_of_inflection_point(self, row=-1):
-	# 	bragg_edge_range = self.parent.fitting_input_dictionary['bragg_edge_range']
-	# 	left_index = np.int(bragg_edge_range[0])
-	# 	right_index = np.int(bragg_edge_range[1])
-	#
-	# 	# get full x_axis (lambda)
-	# 	full_lambda_x_axis = self.parent.fitting_input_dictionary['xaxis']['lambda'][0]
-	# 	lambda_x_axis = full_lambda_x_axis[left_index: right_index]
-	#
-	# 	# get full y_axis (average transmission)
-	# 	full_y_axis = self.parent.fitting_input_dictionary['rois'][row]['profile']
-	# 	y_axis = full_y_axis[left_index: right_index]
-	#
-	# 	# for now inflection is only calculated by using center of selection
-	# 	# FIXME
-	#
-	# 	inflection_point_index = np.int(len(lambda_x_axis) / 2.)
-	# 	return {'left_part': {'lambda_x_axis': lambda_x_axis[0: inflection_point_index],
-	# 	                      'y_axis': y_axis[0: inflection_point_index]},
-	# 	        'right_part': {'lambda_x_axis': lambda_x_axis[inflection_point_index: ],
-	# 	                       'y_axis': y_axis[inflection_point_index: ]},
-	# 	        'inflection_point': {'y': y_axis[inflection_point_index],
-	# 	                             'lambda_x': lambda_x_axis[inflection_point_index]}}
+	def get_a1(self, advanced_mode=True):
+		if advanced_mode:
+			intercept = self.a2_intercept
+			a2 = self.a2
+			a6 = self.a6
+			return intercept + a2 * a6
+		else:
+			all_fitting_axis_dictionary = self.left_center_right_axis
+			y_axis = all_fitting_axis_dictionary['left_part']['y_axis']
+			a1 = np.mean(y_axis)
+			self.a1 = a1
+			return a1
 
 	def get_a2(self, advanced_mode=True):
 		all_fitting_axis_dictionary = self.left_center_right_axis
@@ -166,41 +152,11 @@ class MarchDollaseFittingJobHandler:
 		self.a6 = a6
 		return a6
 
-	def get_a1(self, advanced_mode=True):
-		if advanced_mode:
-			intercept = self.a2_intercept
-			a2 = self.a2
-			a6 = self.a6
-			return intercept + a2 * a6
-		else:
-			all_fitting_axis_dictionary = self.left_center_right_axis
-			y_axis = all_fitting_axis_dictionary['left_part']['y_axis']
-			a1 = np.mean(y_axis)
-			self.a1 = a1
-			return a1
+	def get_sigma(self):
+		return self.parent.march_dollase_fitting_initial_parameters['sigma']
 
-	def get_initial_parameter_value(self, name_of_parameter=None, row=-1):
-		if name_of_parameter == 'd_spacing':
-			return self.get_d_spacing()
-		if name_of_parameter == 'sigma':
-			return self.parent.march_dollase_fitting_initial_parameters['sigma']
-		if name_of_parameter == 'alpha':
-			return self.parent.march_dollase_fitting_initial_parameters['alpha']
-		if name_of_parameter == 'a1':
-			return self.calculate_a1()
-		if name_of_parameter == 'a2':
-			return -1
-		if name_of_parameter == 'a5':
-			return -1
-		if name_of_parameter == 'a6':
-			return -1
-		return None
-
-	def calculate_a1(self):
-		if self.is_advanced_mode():
-			pass
-		else:
-			pass
+	def get_alpha(self):
+		return self.parent.march_dollase_fitting_initial_parameters['alpha']
 
 	def get_d_spacing(self):
 		"""
@@ -220,7 +176,7 @@ class MarchDollaseFittingJobHandler:
 
 	def prepare(self):
 		self.initialize_fitting_input_dictionary()
-		# fitting_input_dictionary = self.parent.fitting_input_dictionary
+		fitting_input_dictionary = self.parent.fitting_input_dictionary
 
 		_is_advanced_mode = self.is_advanced_mode()
 		if _is_advanced_mode:
@@ -235,13 +191,18 @@ class MarchDollaseFittingJobHandler:
 		self.parent.ui.eventProgress.setMaximum(nbr_row_in_fitting_scenario)
 		self.parent.ui.eventProgress.setVisible(True)
 
-		for _row, _row_entry in enumerate(march_dollase_fitting_history_table):
+		nbr_roi_row = len(fitting_input_dictionary['rois'].keys())
+		for _history_row, _row_entry in enumerate(march_dollase_fitting_history_table):
+			print(f"working with history row #{_history_row}")
+
+			for _roi_row in np.arange(nbr_roi_row):
+				print(f"-> working with roi row {_roi_row}")
+				# _entry = fitting_input_dictionary['rois'][_roi_row]['fitting']['march_dollase']
+				# d_spacing_flag =
 
 
 
-
-
-			self.parent.ui.eventProgress.setValue(_row + 1)
+			self.parent.ui.eventProgress.setValue(_history_row + 1)
 			QApplication.processEvents()
 
 		self.parent.ui.eventProgress.setVisible(False)
