1f4cf034ac15015bb9a84dfbbc49aa1df98465de
JeanBilheux <bilheuxjm@ornl.gov>
2020-09-18 16:59:21 -0400

    more work on refactoring project

--- script_for_installation.sh
+++ script_for_installation.sh
@@ -8,6 +8,12 @@ conda install -y -c conda-forge ipywe
 conda install -y -c plotly plotly
 conda install -y -c conda-forge lmfit
 conda install -y -c anaconda pyqtgraph=0.10.0
+conda install -y scikit-image
+conda install -y -c neutrons ipywe
+conda install -y pandas
 
 pip install neutronbraggedge
 pip install NeuNorm
+pip install sectorizedradialprofile
+pip install https://oncat.ornl.gov/packages/pyoncat-1.3.1-py2.py3-none-any.whl
+pip install requests requests-oauthlib
