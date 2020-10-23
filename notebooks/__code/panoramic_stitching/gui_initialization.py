61c70632e97fabc8d558de6ecc8fac519ed680c1
JeanBilheux <bilheuxjm@ornl.gov>
2020-10-23 16:08:12 -0400

    Added pyqtgraph

--- notebooks/__code/panoramic_stitching/gui_initialization.py
+++ notebooks/__code/panoramic_stitching/gui_initialization.py
@@ -1,3 +1,7 @@
+import pyqtgraph as pg
+from qtpy.QtWidgets import QVBoxLayout
+
+
 class GuiInitialization:
 
     def __init__(self, parent=None):
@@ -5,8 +9,17 @@ class GuiInitialization:
 
     def run_all(self):
         self.splitter()
+        self.pyqtgraph()
 
     def splitter(self):
         self.parent.ui.profile_display_splitter.setSizes([500, 500])
         self.parent.ui.display_splitter.setSizes([200, 100])
         self.parent.ui.full_splitter.setSizes([400, 100])
+
+    def pyqtgraph(self):
+        self.parent.ui.image_view = pg.ImageView()
+        self.parent.ui.image_view.ui.roiBtn.hide()
+        self.parent.ui.image_view.ui.menuBtn.hide()
+        image_layout = QVBoxLayout()
+        image_layout.addWidget(self.parent.ui.image_view)
+        self.parent.ui.stitched_widget.setLayout(image_layout)
