45e3700bc8fc5a2456d9266fac15fc047181cff4
JeanBilheux <bilheuxjm@ornl.gov>
2020-11-12 15:01:02 -0500

    export txt files done

--- notebooks/__code/panoramic_stitching_for_tof/export.py
+++ notebooks/__code/panoramic_stitching_for_tof/export.py
@@ -3,11 +3,13 @@ from qtpy.QtWidgets import QFileDialog
 from qtpy import QtGui
 import numpy as np
 from collections import OrderedDict
+import glob
 
 from NeuNorm.normalization import Normalization
 
 from __code.file_handler import make_or_reset_folder
 from __code.panoramic_stitching_for_tof.image_handler import HORIZONTAL_MARGIN, VERTICAL_MARGIN
+from __code.file_handler import copy_and_rename_files_to_folder
 
 FILE_PREFIX = "image_"
 
@@ -27,7 +29,7 @@ class Export:
             self.parent.ui.setEnabled(False)
             QtGui.QGuiApplication.processEvents()
             self.create_panoramic_images()
-            # self.export_images(output_folder=output_folder)
+            self.export_images(output_folder=output_folder)
             self.parent.ui.setEnabled(True)
 
     def create_panoramic_images(self, output_folder=None):
@@ -47,6 +49,8 @@ class Export:
         image_width = self.parent.image_width
 
         panoramic_images_dict = OrderedDict()
+        self.parent.ui.statusbar.showMessage("Create the panoramic images ...")
+        QtGui.QGuiApplication.processEvents()
 
         for _file_index in np.arange(nbr_files):
             panoramic_image = np.zeros((panoramic_height, panoramic_width))
@@ -90,19 +94,41 @@ class Export:
 
         new_folder_name = os.path.basename(self.parent.working_dir) + "_panoramic"
         self.parent.ui.statusbar.showMessage("Exporting images in folder {}".format(new_folder_name))
+        QtGui.QGuiApplication.processEvents()
+
         new_output_folder_name = os.path.join(output_folder, new_folder_name)
 
         make_or_reset_folder(new_output_folder_name)
-        panoramic_images_dict = self.parent.panoramic_images
+        panoramic_images_dict = self.parent.panoramic_images_to_export
 
         list_data = []
         list_filename = []
-        for _key in panoramic_images_dict.keys():
-            list_data.append(panoramic_images_dict[_key])
-            list_filename.append(_key)
+        for _file_name in panoramic_images_dict.keys():
+            list_data.append(panoramic_images_dict[_file_name])
+            list_filename.append(_file_name)
 
         o_norm = Normalization()
         o_norm.load(data=list_data)
         o_norm.data['sample']['filename'] = list_filename
         o_norm.export(new_output_folder_name, data_type='sample')
+
+        self.copy_txt_files_to_output_folder(output_folder=new_folder_name)
+
         self.parent.ui.statusbar.showMessage("{} has been created!".format(new_output_folder_name), 10000)  # 10s
+        QtGui.QGuiApplication.processEvents()
+
+    def copy_txt_files_to_output_folder(self, output_folder=None):
+        list_input_folders = self.parent.list_folders
+        first_folder = list_input_folders[0]
+        list_txt_files = glob.glob(first_folder + "/*.txt")
+
+        list_new_file_names = []
+
+        for txt_file in list_txt_files:
+            split_name = os.path.basename(txt_file).split("_")
+            new_name = split_name[-1]
+            list_new_file_names.append(new_name)
+
+        copy_and_rename_files_to_folder(list_files=list_txt_files,
+                                        new_list_files_names=list_new_file_names,
+                                        output_folder=output_folder)
