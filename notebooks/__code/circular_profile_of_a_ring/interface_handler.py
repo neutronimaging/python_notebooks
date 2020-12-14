40722d9bc5af0accaa11936bb93b4753169370b6
JeanBilheux <bilheuxjm@ornl.gov>
2020-12-14 13:50:49 -0500

    Initialization and display of pyqtgraph. this refs #14

--- notebooks/__code/circular_profile_of_a_ring/interface_handler.py
+++ notebooks/__code/circular_profile_of_a_ring/interface_handler.py
@@ -1,5 +1,8 @@
 import os
 from qtpy.QtWidgets import QMainWindow
+import pyqtgraph as pg
+from qtpy.QtWidgets import QVBoxLayout
+import numpy as np
 
 from __code import load_ui
 
@@ -7,12 +10,15 @@ from __code import load_ui
 class InterfaceHandler:
 
     def __init__(self, working_dir=None, o_norm=None):
-        o_interface = Interface(data=o_norm.data['sample'],
+        o_interface = Interface(data=o_norm.data['sample']['data'],
                                 working_dir=working_dir)
         o_interface.show()
 
 
 class Interface(QMainWindow):
+
+    histogram_level = None
+    current_live_image = None
 
     def __init__(self, parent=None, data=None, working_dir=None):
 
@@ -26,6 +32,22 @@ class Interface(QMainWindow):
                                                  'ui_circular_profile_of_a_ring.ui'))
         self.ui = load_ui(ui_full_path, baseinstance=self)
         self.setWindowTitle("Circular Profile of a Ring")
+
+        self.init_widgets()
+        self.slider_image_changed(new_index=0)
+
+    def init_widgets(self):
+        self.ui.image_view = pg.ImageView(view=pg.PlotItem())
+        self.ui.image_view.ui.roiBtn.hide()
+        self.ui.image_view.ui.menuBtn.hide()
+        image_layout = QVBoxLayout()
+        image_layout.addWidget(self.ui.image_view)
+        self.ui.widget.setLayout(image_layout)
+
+        self.ui.splitter.setSizes([500, 200])
+
+        nbr_files = len(self.data)
+        self.ui.image_slider.setMaximum(nbr_files-1)
 
     def help_button_clicked(self):
         pass
@@ -53,4 +75,25 @@ class Interface(QMainWindow):
 
     def calculate_profiles_clicked(self):
         pass
-    
+
+    def slider_image_changed(self, new_index=0):
+
+        _view = self.ui.image_view.getView()
+        _view_box = _view.getViewBox()
+        _state = _view_box.getState()
+
+        first_update = False
+        if self.histogram_level is None:
+            first_update = True
+        _histo_widget = self.ui.image_view.getHistogramWidget()
+        self.histogram_level = _histo_widget.getLevels()
+
+        data = self.data[new_index]
+        _image = np.transpose(data)
+        self.ui.image_view.setImage(_image)
+        self.current_live_image = _image
+
+        _view_box.setState(_state)
+        if not first_update:
+            _histo_widget.setLevels(self.histogram_level[0],
+                                    self.histogram_level[1])
