a22db7d7ef125ab3ad8c3dcd09a337e7e3e3fc02
JeanBilheux <bilheuxjm@ornl.gov>
2021-05-19 17:58:33 -0400

    adding code to retrieve metadata of interest + unit test. this refs #32

--- notebooks/tests/_utilities/test_file.py
+++ notebooks/tests/_utilities/test_file.py
@@ -12,9 +12,12 @@ class TestFolders:
 
     def test_retrieving_metadata(self):
         metadata_value_1 = "#base folder"
-        value_returned = file.retrieve_metadata_value_from_ascii_file(filename=self.ascii_file_name,
+        value_returned_1 = file.retrieve_metadata_value_from_ascii_file(filename=self.ascii_file_name,
                                                                       metadata_name=metadata_value_1)
         value_expected = "/Users/j35/IPTS/VENUS/IPTS-25778_normalized"
-        assert value_expected == value_returned
+        assert value_expected == value_returned_1
 
         metadata_value_2 = "base folder"
+        value_returned_2 = file.retrieve_metadata_value_from_ascii_file(filename=self.ascii_file_name,
+                                                                      metadata_name=metadata_value_2)
+        assert value_expected == value_returned_2
