e2f2aa3b7e687c22813dda97594650f7d40280b1
JeanBilheux <bilheuxjm@ornl.gov>
2021-06-08 17:49:07 -0400

    Refactoring code. this refs #44

---
+++ notebooks/__code/metadata_overlapping_images/__init__.py
@@ -0,0 +1,2 @@
+HELP_PAGE = "https://neutronimaging.pages.ornl.gov/en/tutorial/notebooks/metadata_overlapping_images/"
+LIST_FUNNY_CHARACTERS = "~!@#$%^&*()_+<>?,./'{}|[]\\"
---
+++ notebooks/__code/metadata_overlapping_images/general_classes.py
@@ -0,0 +1,17 @@
+
+class ScaleSettings:
+    x0 = 50
+    y0 = 50
+    thickness = 10
+
+    cursor_width = 10
+    cursor_height = 10
+
+    color = [255, 255, 255]  # white
+
+
+class MetadataSettings:
+    x0 = 200
+    y0 = 200
+
+    color = [255, 255, 255] # white
