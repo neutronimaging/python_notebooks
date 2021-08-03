68b4c3c265e83c116db10246264a5b34e20b7fe6
JeanBilheux <bilheuxjm@ornl.gov>
2021-08-03 10:16:14 -0400

    Program can now load jpg using matplotlib library. Bug reported in Pillow. this refs #58

--- notebooks/__code/extract_evenly_spaced_files/event_handler.py
+++ notebooks/__code/extract_evenly_spaced_files/event_handler.py
@@ -4,10 +4,13 @@ from qtpy import QtGui
 import numpy as np
 import logging
 import os
+# from PIL import Image
+import matplotlib.image as mpimg
 
 from NeuNorm.normalization import Normalization
 
 from __code._utilities.list_widget import ListWidget
+from __code._utilities.file import get_file_extension
 from __code._utilities.status_message import StatusMessageStatus, show_status_message
 
 
@@ -17,9 +20,19 @@ class EventHandler:
         self.parent = parent
 
     def load_file(self, file=None):
-        o_norm = Normalization()
-        o_norm.load(file=file, notebook=False)
-        _data = o_norm.data['sample']['data'][0]
+        ext = get_file_extension(filename=file)
+
+        if ext.lower() in ['jpg', 'jpeg']:
+            _data = mpimg.imread(file)
+            # with Image.open(file) as im:   # BUG that crashes the UI
+            #    _data = np.asarray(im.convert('L'))/255.
+
+        else:
+            o_norm = Normalization()
+            o_norm.load(file=file, notebook=False)
+            _data = o_norm.data['sample']['data'][0]
+
+        logging.info(f"np.shape(_data): {np.shape(_data)}")
         return _data
 
     def load_files(self):
