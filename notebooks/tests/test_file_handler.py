704a05dd90746a61f3ddfc7c380f6366ffb00c07
JeanBilheux <bilheuxjm@ornl.gov>
2021-08-24 13:26:16 -0400

    Testing if analysis notebooks are updated.

--- notebooks/tests/test_file_handler.py
+++ notebooks/tests/test_file_handler.py
@@ -25,6 +25,7 @@ class TestReadBraggEdgeFittingAsciiFormat:
         print(f"glob(data_path): {glob.glob(str(data_path) + '/data/*')}")
         my_data_path = "/home/runner/work/python_notebooks/notebooks/tests/data/"
         print(f"glob(my_data_path): {glob.glob(my_data_path)}")
+        print("in master")
 
         result = read_bragg_edge_fitting_ascii_format(full_file_name=self.ascii_file_name)
         metadata_column = result['metadata']['columns']
