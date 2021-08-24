191c887d0ccf756d64b4dbc95d55f319df266ca6
JeanBilheux <bilheuxjm@ornl.gov>
2021-08-24 13:25:31 -0400

    More testing. this refs #59

--- notebooks/tests/test_file_handler.py
+++ notebooks/tests/test_file_handler.py
@@ -26,11 +26,11 @@ class TestReadBraggEdgeFittingAsciiFormat:
         print(f"glob(data_path): {glob.glob(str(data_path) + '/data/*')}")
         my_data_path = "/home/runner/work/python_notebooks/python_notebooks/notebooks/tests/my_data/"
         print(f"glob(my_data_path): {glob.glob(my_data_path)}")
-
-        result = read_bragg_edge_fitting_ascii_format(full_file_name=self.ascii_file_name)
         print(f"str(Path(__file__).parent: {str(Path(__file__).parent)}")
         print(f"str(Path(data_path): {str(Path(self.data_path))}")
         print(f"self.ascii_file_name: {self.ascii_file_name}")
+
+        result = read_bragg_edge_fitting_ascii_format(full_file_name=self.ascii_file_name)
         assert False
         metadata_column = result['metadata']['columns']
 
@@ -52,7 +52,6 @@ class TestReadBraggEdgeFittingAsciiFormat:
                 'a6'       : 'None', 'd_spacing_error': 'None', 'sigma_error': 'None', 'alpha_error': 'None',
                 'a1_error' : 'None', 'a2_error': 'None', 'a5_error': 'None', 'a6_error': 'None'}}
         assert len(metadata_column) == 86
-        assert False
 
     def test_retrieving_data(self):
         result = read_bragg_edge_fitting_ascii_format(full_file_name=self.ascii_file_name)
