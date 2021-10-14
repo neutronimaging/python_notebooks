92f1972c32639a9ccdecb2a21be08096af071bad
JeanBilheux <bilheuxjm@ornl.gov>
2021-10-14 15:28:07 -0400

    cleaning of metadata 1 and 2 seems to be working. this refs #71

--- notebooks/__code/metadata_overlapping_images/utilities.py
+++ notebooks/__code/metadata_overlapping_images/utilities.py
@@ -22,7 +22,7 @@ def string_cleaning(first_part_of_string_to_remove="", last_part_of_string_to_re
 
     regular_expr = r"{}(.*){}".format(_clean_first_part, _clean_second_part)
 
-    m = re.match(regular_expr, string_to_clean)
+    m = re.match(regular_expr, str(string_to_clean))
     if m and m.group(1):
         _new_str = m.group(1)
         return _new_str
@@ -31,12 +31,38 @@ def string_cleaning(first_part_of_string_to_remove="", last_part_of_string_to_re
 
 
 def linear_operation(input_parameter="", math_1="", value_1="", math_2="", value_2=""):
-    operation_to_eval = f"{input_parameter}"
-    if value_1:
-        operation_to_eval += f" {math_1} {float(value_1)}"
-    if value_2:
-        operation_to_eval += f" {math_2} {float(value_2)}"
 
-    result = eval(operation_to_eval)
+    if is_linear_operation_valid(input_parameter=input_parameter,
+                                 value_1=value_1,
+                                 value_2=value_2):
 
-    return result
+        operation_to_eval = f"{input_parameter}"
+        if value_1:
+            operation_to_eval += f" {math_1} {float(value_1)}"
+        if value_2:
+            operation_to_eval += f" {math_2} {float(value_2)}"
+
+        result = eval(operation_to_eval)
+        return result
+
+    else:
+        return ""
+
+
+def is_linear_operation_valid(input_parameter="", value_1="", value_2=""):
+
+    def is_error_in_operation(value):
+        operation = str(value.strip())
+        if operation:
+            try:
+                float(operation)
+            except ValueError:
+                return True
+        return False
+
+    if (not is_error_in_operation(value_1)) and \
+        (not is_error_in_operation(value_2)) and \
+        (not is_error_in_operation(input_parameter)):
+        return True
+
+    return False
