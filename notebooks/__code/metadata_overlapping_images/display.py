37eb9b9f3b3a724bf7b375349438d80972afe703
JeanBilheux <bilheuxjm@ornl.gov>
2021-10-18 10:26:52 -0400

    fixed vertical line in graph. this refs #71

--- notebooks/__code/metadata_overlapping_images/display.py
+++ notebooks/__code/metadata_overlapping_images/display.py
@@ -346,15 +346,12 @@ class DisplayMetadataPyqtUi:
             current_index = self.parent.ui.file_slider.value()
             _pen = pg.mkPen((255, 0, 0), width=4)
 
-            print(f"np.shape(clean_and_format_x_axis): {np.shape(clean_and_format_x_axis)}")
-            print(f"np.shape(clean_and_format_y_axis): {np.shape(clean_and_format_y_axis)}")
-
             graph.plot(x=[clean_and_format_x_axis[current_index]],
                        y=[clean_and_format_y_axis[current_index]],
                        pen=_pen,
                        symboBrush=(255, 0, 0),
                        symbolPen='w')
-            _inf_line = pg.InfiniteLine(current_index, pen=_pen)
+            _inf_line = pg.InfiniteLine(clean_and_format_x_axis[current_index], pen=_pen)
             graph.addItem(_inf_line)
 
             x0 = self.parent.ui.graph_position_x.value()
