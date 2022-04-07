22f014effe1b7187f5ae1ad5dd019e9713e3c8e5
JeanBilheux <bilheuxjm@ornl.gov>
2022-04-07 16:13:54 -0400

    feature implemented. this refs #31

--- notebooks/__code/super_user.py
+++ notebooks/__code/super_user.py
@@ -1,9 +1,15 @@
 from ipywidgets import widgets
-from IPython.core.display import display, HTML
+from IPython.core.display import display
 import os
 
 from .config import password_to_unlock_config as PASSWORD
 from .config import debugger_folder as list_debugging_folder
+from .config import debugging
+from .config import percentage_of_images_to_use_for_roi_selection as PERCENTAGE_OF_IMAGES
+from __code._utilities.file import read_ascii, make_ascii_file_from_string
+
+THIS_FILE_PATH = os.path.dirname(__file__)
+CONFIG_FILE = os.path.join(THIS_FILE_PATH, 'config.py')
 
 
 class SuperUser:
@@ -21,7 +27,8 @@ class SuperUser:
 
         # ----
 
-        self.debugging_mode = widgets.Checkbox(value=False,
+        # debugging mode
+        self.debugging_mode = widgets.Checkbox(value=debugging,
                                                description="Debugging Mode",
                                                disabled=True)
 
@@ -33,6 +40,7 @@ class SuperUser:
                                                layout=widgets.Layout(height="200px",
                                                                      width="400px"))
 
+        # list of debugging folders
         self.remove_entry = widgets.Button(description="Remove Selected Entry",
                                            disabled=True,
                                            button_style="",
@@ -49,7 +57,20 @@ class SuperUser:
                                         icon="plus-square",
                                         layout=widgets.Layout(width="95px"))
         self.add_entry.on_click(self.add_entry_clicked)
-        hori_layout = widgets.HBox([self.new_entry_text, self.add_entry])
+        hori_layout_percentage = widgets.HBox([self.new_entry_text, self.add_entry])
+
+        # percentage of images to use for roi selection
+        self.percentage_roi_label = widgets.HTML("Percentage of images to use for ROI selection",
+                                                 disabled=True)
+        percentage_of_images = PERCENTAGE_OF_IMAGES * 100
+        self.percentage_roi_value = widgets.FloatText(value=percentage_of_images,
+                                                      disabled=True,
+                                                      layout=widgets.Layout(width="50px"))
+        self.percentage_units = widgets.HTML("%",
+                                             disabled=True)
+        hori_layout = widgets.HBox([self.percentage_roi_label,
+                                    self.percentage_roi_value,
+                                    self.percentage_units])
 
         # ----
         self.save_changes = widgets.Button(description="Save Changes",
@@ -57,6 +78,7 @@ class SuperUser:
                                            button_style='Success',
                                            layout=widgets.Layout(width="400px"),
                                            icon='floppy-o')
+        self.save_changes.on_click(self.save_button_clicked)
 
         vertical_layout = widgets.VBox([password,
                                         widgets.HTML("<hr>"),
@@ -65,11 +87,15 @@ class SuperUser:
                                         self.debugging_folder_label,
                                         self.debugging_folder,
                                         self.remove_entry,
+                                        hori_layout_percentage,
+                                        widgets.HTML("<br>"),
                                         hori_layout,
                                         widgets.HTML("<hr>"),
                                         self.save_changes])
 
         display(vertical_layout)
+
+        self.update_widgets()
 
     def remove_entry_clicked(self, state):
         value = self.debugging_folder.value
@@ -110,6 +136,36 @@ class SuperUser:
                    self.remove_entry,
                    self.add_entry,
                    self.new_entry_text,
+                   self.percentage_roi_label,
+                   self.percentage_roi_value,
+                   self.percentage_units,
                    ]
         for _ui in list_ui:
             _ui.disabled = disabled
+
+    def save_button_clicked(self, value):
+        debugging_mode = self.debugging_mode.value
+
+        list_folders = self.debugging_folder.options
+        str_list_folders = [f"{_folder}" for _folder in list_folders]
+        str_list_folders_formatted = f"{str_list_folders}"
+
+        percentage_roi_selection = self.percentage_roi_value.value / 100.
+
+        ascii = read_ascii(CONFIG_FILE)
+        ascii_split = ascii.split("\n")
+
+        ascii_after = []
+        for _line in ascii_split:
+            if "debugging = " in _line:
+                ascii_after.append(f"debugging = {debugging_mode}")
+            elif "debugger_folder = " in _line:
+                ascii_after.append(f"debugger_folder = {str_list_folders_formatted}")
+            elif "percentage_of_images_to_use_for_roi_selection = " in _line:
+                ascii_after.append(f"percentage_of_images_to_use_for_roi_selection = {percentage_roi_selection}")
+            else:
+                ascii_after.append(_line)
+
+        ascii_after_formatted = "\n".join(ascii_after)
+        make_ascii_file_from_string(text=ascii_after_formatted,
+                                    filename=CONFIG_FILE)
