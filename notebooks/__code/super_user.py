90846614314ee5d3b934383e71d30927f02142eb
JeanBilheux <bilheuxjm@ornl.gov>
2022-04-07 14:49:39 -0400

    debugging folders. this refs #31

--- notebooks/__code/super_user.py
+++ notebooks/__code/super_user.py
@@ -1,5 +1,6 @@
 from ipywidgets import widgets
 from IPython.core.display import display, HTML
+import os
 
 from .config import password_to_unlock_config as PASSWORD
 from .config import debugger_folder as list_debugging_folder
@@ -21,8 +22,8 @@ class SuperUser:
         # ----
 
         self.debugging_mode = widgets.Checkbox(value=False,
-                                              description="Debugging Mode",
-                                              disabled=True)
+                                               description="Debugging Mode",
+                                               disabled=True)
 
         self.debugging_folder_label = widgets.HTML("List of folders to look for when running in <b>debugging</b> mode.")
         self.debugging_folder = widgets.Select(options=list_debugging_folder,
@@ -32,10 +33,29 @@ class SuperUser:
                                                layout=widgets.Layout(height="200px",
                                                                      width="400px"))
 
+        self.remove_entry = widgets.Button(description="Remove Selected Entry",
+                                           disabled=True,
+                                           button_style="",
+                                           icon='minus-square',
+                                           layout=widgets.Layout(width="400px"))
+        self.remove_entry.on_click(self.remove_entry_clicked)
+
+        self.new_entry_text = widgets.Text(value="",
+                                           description="New folder",
+                                           disabled=True)
+        self.add_entry = widgets.Button(description="",
+                                        disabled=True,
+                                        button_style="",
+                                        icon="plus-square",
+                                        layout=widgets.Layout(width="95px"))
+        self.add_entry.on_click(self.add_entry_clicked)
+        hori_layout = widgets.HBox([self.new_entry_text, self.add_entry])
+
         # ----
         self.save_changes = widgets.Button(description="Save Changes",
                                            disabled=True,
                                            button_style='Success',
+                                           layout=widgets.Layout(width="400px"),
                                            icon='floppy-o')
 
         vertical_layout = widgets.VBox([password,
@@ -44,13 +64,36 @@ class SuperUser:
                                         widgets.HTML("<br>"),
                                         self.debugging_folder_label,
                                         self.debugging_folder,
+                                        self.remove_entry,
+                                        hori_layout,
                                         widgets.HTML("<hr>"),
                                         self.save_changes])
 
-
         display(vertical_layout)
 
+    def remove_entry_clicked(self, state):
+        value = self.debugging_folder.value
+        options = self.debugging_folder.options
+        options_to_keep = []
+        for _option in options:
+            if value == _option:
+                continue
+            else:
+                options_to_keep.append(_option)
+        self.debugging_folder.options = options_to_keep
 
+    def add_entry_clicked(self, state):
+        entry_to_add = self.new_entry_text.value
+        options = list(self.debugging_folder.options)
+
+        if entry_to_add in options:
+            self.new_entry_text.value = ""
+            return
+
+        if os.path.exists(entry_to_add):
+            options.append(entry_to_add)
+        self.debugging_folder.options = options
+        self.new_entry_text.value = ""
 
     def password_entered(self, value):
         new_password = value['new']
@@ -61,9 +104,12 @@ class SuperUser:
         self.update_widgets(disabled=disabled)
 
     def update_widgets(self, disabled=True):
-        list_ui = [ self.debugging_mode,
-                    self.save_changes,
-                    self.debugging_folder
-                    ]
+        list_ui = [self.debugging_mode,
+                   self.save_changes,
+                   self.debugging_folder,
+                   self.remove_entry,
+                   self.add_entry,
+                   self.new_entry_text,
+                   ]
         for _ui in list_ui:
             _ui.disabled = disabled
