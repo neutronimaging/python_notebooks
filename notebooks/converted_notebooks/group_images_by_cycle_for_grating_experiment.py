9989343858b8d70a4a569f1345755a20069923ac
JeanBilheux <bilheuxjm@ornl.gov>
2022-04-26 07:46:20 -0400

    ready to be deployed and tested, after merging to master. this refs #94

--- notebooks/converted_notebooks/group_images_by_cycle_for_grating_experiment.py
+++ notebooks/converted_notebooks/group_images_by_cycle_for_grating_experiment.py
@@ -19,6 +19,9 @@
 # # Select Your IPTS 
 
 # + run_control={"frozen": false, "read_only": false}
+import warnings
+warnings.filterwarnings('ignore')
+
 from __code.group_images_by_cycle_for_grating_experiment.group_images import GroupImages
 from __code import system
 system.System.select_working_dir()
@@ -26,11 +29,11 @@ from __code.__all import custom_style
 custom_style.style()
 
 # + [markdown] run_control={"frozen": false, "read_only": false}
-# # Select Input Folder
+# # Select Data to Sort
 
 # + run_control={"frozen": false, "read_only": false}
 o_group = GroupImages(working_dir=system.System.get_working_dir())
-o_group.select_input_folder()
+o_group.select_data_to_sort()
 # -
 
 # # Select Metadata to Use for Sorting 
@@ -50,4 +53,8 @@ o_group.grouping()
 o_group.select_output_folder()
 # -
 
+# # Generate Angel Configuration File (Excel) 
 
+# %gui qt
+
+o_group.generate_angel_configuration_file()
