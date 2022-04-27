c6cd2d413b9a96d4a4e56604bd001c80bb35416c
JeanBilheux <bilheuxjm@ornl.gov>
2022-04-27 09:48:58 -0400

    fixing button interaction in excel ui. this refs #94

--- notebooks/__code/group_images_by_cycle_for_grating_experiment/repeat_widget_change_dialog.py
+++ notebooks/__code/group_images_by_cycle_for_grating_experiment/repeat_widget_change_dialog.py
@@ -1,4 +1,4 @@
-from qtpy.QtWidgets import QDialog
+from qtpy.QtWidgets import QMainWindow
 from qtpy.QtGui import QIcon
 from qtpy import QtCore, QtGui
 import os
@@ -6,7 +6,7 @@ import os
 from __code import load_ui
 
 
-class RepeatWidgetChangeDialog(QDialog):
+class RepeatWidgetChangeDialog(QMainWindow):
 
     def __init__(self, parent=None):
 
@@ -18,25 +18,34 @@ class RepeatWidgetChangeDialog(QDialog):
                                                  'ui_grating_excel_widget_dialog.ui'))
         self.ui = load_ui(ui_full_path, baseinstance=self)
 
-        # self.init_widgets()
+        self.init_widgets()
 
     def init_widgets(self):
-        file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "static/")
+        
+        statis_file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "static")
+        self.do_not_repeat_released_file = os.path.join(statis_file_path, "do_not_repeat_button_released.png")
+        self.do_not_repeat_pressed_file = os.path.join(statis_file_path, "do_not_repeat_button_pressed.png")
+        self.repeat_released_file = os.path.join(statis_file_path, "repeat_off.png")
+        self.repeat_pressed_file = os.path.join(statis_file_path, "repeat_on.png")
 
-        no_repeat_icon = QIcon()
-        no_repeat_icon.addPixmap(QtGui.QPixmap(os.path.join(file_path, "do_no_repeat_off.png")), QtGui.QIcon.Active,
-                               QtGui.QIcon.Off)
-        no_repeat_icon.addPixmap(QtGui.QPixmap(os.path.join(file_path, "do_not_repeat_on.png")), QtGui.QIcon.Active,
-                                 QtGui.QIcon.On)
+        no_repeat_icon = QIcon(self.do_not_repeat_released_file)
         self.ui.do_not_repeat_pushButton.setIcon(no_repeat_icon)
-        self.ui.do_not_repeat_pushButton.setStyleSheet("")
-        self.ui.do_not_repeat_pushButton.setIconSize(QtCore.QSize(198, 204))
+        self.ui.do_not_repeat_pushButton.setIconSize(QtCore.QSize(204, 198))
 
-        repeat_icon = QIcon()
-        repeat_icon.addPixmap(QtGui.QPixmap(os.path.join(file_path, "repeat_off.png")), QtGui.QIcon.Active,
-                              QtGui.QIcon.Off)
-        repeat_icon.addPixmap(QtGui.QPixmap(os.path.join(file_path, "repeat_on.png")), QtGui.QIcon.Active,
-                              QtGui.QIcon.On)
+        repeat_icon = QIcon(self.repeat_released_file)
         self.ui.repeat_pushButton.setIcon(repeat_icon)
-        self.ui.repeat_pushButton.setIconSize(QtCore.QSize(198, 204))
-        self.ui.repeat_pushButton.setStyleSheet("")
+        self.ui.repeat_pushButton.setIconSize(QtCore.QSize(204, 198))
+
+    def do_not_repeat_pressed(self):
+        no_repeat_icon = QIcon(self.do_not_repeat_pressed_file)
+        self.ui.do_not_repeat_pushButton.setIcon(no_repeat_icon)
+
+    def do_not_repeat_released(self):
+        no_repeat_icon = QIcon(self.do_not_repeat_released_file)
+        self.ui.do_not_repeat_pushButton.setIcon(no_repeat_icon)
+
+    def repeat_pressed(self):
+        pass
+
+    def repeat_released(self):
+        pass
