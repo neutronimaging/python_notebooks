d39ffac01dce349e093e1be28b3ffb478e420e7a
JeanBilheux <bilheuxjm@ornl.gov>
2022-11-07 15:06:46 -0500

    fixed config for users. this refs #115

--- notebooks/__code/config.py
+++ notebooks/__code/config.py
@@ -1,6 +1,6 @@
-debugging = False
+debugging = True
 debugger_username = 'j35'
-debugger_folder = ['/Volumes/G-DRIVE/IPTS/',
+debugger_folder = ['/Volumes/G-DRIVE/IPTS/', '/Users/j35/HFIR/',
                    '/Volumes/Buffalo/IPTS/', '/Users/j35/IPTS/',
                    '/Volumes/my_book_thunderbolt_duo/IPTS/']
 project_folder = 'IPTS_16259'
