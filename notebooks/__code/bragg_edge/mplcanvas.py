ff7552e0a03fbd1d1173759fec7a00e2f6451dd3
JeanBilheux <bilheuxjm@ornl.gov>
2022-12-16 11:11:57 -0500

    Added notebook that check all imports. this refs #118

---
+++ notebooks/__code/bragg_edge/mplcanvas.py
@@ -0,0 +1,14 @@
+import matplotlib
+matplotlib.use('Qt5Agg')
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from matplotlib.figure import Figure
+
+
+class MplCanvas(FigureCanvasQTAgg):
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        self.fig = fig
+        super(MplCanvas, self).__init__(fig)
