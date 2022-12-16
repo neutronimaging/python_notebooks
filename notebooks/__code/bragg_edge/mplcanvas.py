69245e188af2d2a40cd11b0ed24f2f77b9afa837
JeanBilheux <bilheuxjm@ornl.gov>
2022-12-16 12:12:50 -0500

    fixed typo. this refs #118

--- notebooks/__code/bragg_edge/mplcanvas.py
+++ notebooks/__code/bragg_edge/mplcanvas.py
@@ -7,9 +7,9 @@ from matplotlib.figure import Figure
 
 class MplCanvas(FigureCanvasQTAgg):
 
-	def __init__(self, parent=None, width=5, height=4, dpi=100):
-		fig = Figure(figsize=(width, height), dpi=dpi)
-		self.axes = fig.add_subplot(111)
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
         self.fig = fig
-		super(MplCanvas, self).__init__(fig)
+        super(MplCanvas, self).__init__(fig)
 
