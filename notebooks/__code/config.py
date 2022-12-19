f6bb9826fd2b174b20eb68a1be9efdef96fe352c
JeanBilheux <bilheuxjm@ornl.gov>
2022-12-19 11:15:31 -0500

    ready to be deployed and tested. this refs #119

--- notebooks/__code/config.py
+++ notebooks/__code/config.py
@@ -1,4 +1,4 @@
-debugging = True
+debugging = False
 debugger_username = 'j35'
 debugger_folder = ['/Volumes/G-DRIVE/IPTS/', '/Users/j35/HFIR/',
                    '/Volumes/Buffalo/IPTS/', '/Users/j35/IPTS/',
