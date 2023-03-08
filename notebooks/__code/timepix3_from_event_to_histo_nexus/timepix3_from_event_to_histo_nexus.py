a3140f61167c7b7a38bf0c2b565270847268509e
JeanBilheux <bilheuxjm@ornl.gov>
2023-03-08 13:25:35 -0500

    loading h5. binning. display metadata. display integrated signal. this refs #127

--- notebooks/__code/timepix3_from_event_to_histo_nexus/timepix3_from_event_to_histo_nexus.py
+++ notebooks/__code/timepix3_from_event_to_histo_nexus/timepix3_from_event_to_histo_nexus.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from ipywidgets import interactive
 import ipywidgets as widgets
 from IPython.core.display import display, HTML
+import algotom.io.loadersaver as losa
 
 from neutronbraggedge.experiment_handler import *
 from neutronbraggedge.braggedge import BraggEdge as BraggEdgeLibrary
@@ -16,7 +17,7 @@ from __code.ipywe import fileselector
 LOG_FILE_NAME = ".timepix3_event_nexus.log"
 
 
-class Timepix3:
+class Timepix3FromEventToHistoNexus:
     # histogram data
     histo_data = None
 
@@ -36,26 +37,17 @@ class Timepix3:
                             level=logging.INFO)
         logging.info("*** Starting a new session ***")
 
-    def select_nexus(self):
+    def select_event_nexus(self):
         self.nexus_ui = fileselector.FileSelectorPanel(instruction='Select NeXus file ...',
                                                        start_dir=self.working_dir,
-                                                       next=self.load_nexus,
+                                                       next=self.display_tree_structure,
                                                        filters={'NeXus': ".h5"},
                                                        multiple=False)
         self.nexus_ui.show()
 
-    def load_nexus(self, nexus_file_name=None):
-        logging.info(f"Loading NeXus: {nexus_file_name}")
-        with h5py.File(nexus_file_name, 'r') as f:
-            try:
-                logging.info("Loading a MCP event file!")
-                self.x_array = np.array(f['events']['x'])
-                self.y_array = np.array(f['events']['y'])
-                self.tof_ns_array = np.array(f['events']['tof_ns'])
-                self.mode = 'mcp'
-            except KeyError:
-                logging.info("Nexus file not supported! Only working with MCP data set")
-                print("Only works with the MCP data set created by Su-Ann")
+    def display_tree_structure(self, nexus_file_name=None):
+        logging.info(f"Display tree structure of {nexus_file_name}")
+        losa.get_hdf_tree(nexus_file_name)
 
     def collect_metadata(self):
         nbr_events = len(self.x_array)
@@ -63,8 +55,8 @@ class Timepix3:
         max_x = np.max(self.x_array)
         min_y = np.min(self.y_array)
         max_y = np.max(self.y_array)
-        min_tof = np.min(self.tof_ns_array)
-        max_tof = np.max(self.tof_ns_array)
+        min_tof = np.min(self.tof_array)
+        max_tof = np.max(self.tof_array)
 
         self.metadata = {'nbr_events': nbr_events,
                          'x': {'min': min_x,
@@ -98,17 +90,114 @@ class Timepix3:
 
         vbox = widgets.VBox([widgets.Label("Metadata"),
                              widgets.Textarea(value=metadata,
-                                              disabled=True)])
+                                              disabled=True,
+                                              layout=widgets.Layout(height='200px'))])
         display(vbox)
 
+    def define_detector(self):
+        self.width_ui = widgets.IntText(value=1024,
+                                        description="Width")
+        self.height_ui = widgets.IntText(value=1024,
+                                         description="Height")
+        vbox = widgets.VBox([widgets.Label("MCP detector size:"),
+                             self.height_ui,
+                             self.width_ui])
+        display(vbox)
 
+    def select_binning_parameter(self):
 
+        self.nbr_bin_ui = widgets.IntText(value=1000,
+                                  description="Nbr of bins:")
+        display(self.nbr_bin_ui)
 
+        self.range_to_use = widgets.IntSlider(value=50,
+                                              max=100,
+                                              min=1,
+                                              description="% to use")
+        display(self.range_to_use)
 
+    def bins(self):
+        logging.info("Binning the data:")
+        bin_value = self.nbr_bin_ui.value
+        percentage_to_use = self.range_to_use.value
 
+        nbr_point = len(self.tof_array)
+        max_index_to_use = int(nbr_point * (percentage_to_use / 100))
 
+        mcp_height = self.height_ui.value
+        mcp_width = self.width_ui.value
 
+        logging.info(f"\tbin_value: {bin_value}")
+        logging.info(f"\tpercentage to use: {percentage_to_use}")
+        logging.info(f"\tnbr point: {nbr_point}")
+        logging.info(f"\tmax_index_to_use: {max_index_to_use}")
+        logging.info(f"\tMCP height: {mcp_height}")
+        logging.info(f"\tMCP width: {mcp_width}")
 
+        logging.info(f"Narrowing down the arrays to the range specified!")
+        tof_array = self.tof_array[0:max_index_to_use]
+        x_array = self.x_array[0:max_index_to_use]
+        y_array = self.y_array[0:max_index_to_use]
+
+        # indexes with NaN
+        logging.info(f"Removing NaNs!")
+        index_nan = np.where(np.isnan(tof_array))
+        # remove those x, y and tof_nx
+        self.tof_array_cleaned = np.delete(tof_array, index_nan)
+        self.x_array_cleaned = np.delete(x_array, index_nan)
+        self.y_array_cleaned = np.delete(y_array, index_nan)
+
+        # binning
+        logging.info(f"Binning of tof array ... started")
+        histo_tof, bins_tof = np.histogram(self.tof_array_cleaned, bin_value)
+        logging.info(f"Binning of tof array ...  Done!")
+
+        stack_images = np.zeros((bin_value, mcp_height, mcp_width))
+        np.shape(stack_images)
+
+        for _index, _tof in enumerate(tof_array):
+            _array_location = np.where((bins_tof - _tof) < 0)
+            try:
+                _location = _array_location[0][-1]
+            except IndexError:
+                _location = 0
+            x = round(x_array[_index])
+            y = round(y_array[_index])
+            if (x < 0) or (y < 0) or (x > 1023) or (y > 1023):
+                continue
+
+            stack_images[_location, y, x] += 1
+
+        self.stack_images = stack_images
+
+    def display_integrated_stack(self):
+
+        integrated_stack = self.stack_images.sum(axis=0)
+        fig, ax = plt.subplots(figsize=(7, 7),
+                               nrows=1, ncols=1)
+        image = ax.imshow(integrated_stack)
+        self.cb = plt.colorbar(image, ax=ax)
+        plt.show()
+
+        max_counts = np.max(integrated_stack)
+
+        def plot_integrated(vmin, vmax):
+            self.cb.remove()
+            plt.title("Integrated slices (sum)")
+            image = ax.imshow(integrated_stack, vmin=vmin, vmax=vmax)
+            self.cb = plt.colorbar(image, ax=ax)
+            plt.show()
+
+        v = interactive(plot_integrated,
+                        vmin=widgets.IntSlider(min=0,
+                                                 max=max_counts,
+                                                 value=0),
+                        vmax=widgets.IntSlider(min=0,
+                                               max=max_counts,
+                                               value=max_counts),
+                        )
+
+        display(v)
 
 
     def rebin_and_display_data(self):
