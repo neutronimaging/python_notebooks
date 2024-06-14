ab9bbbdb65b0a5b81c79d41627aa6a88a780e5dd
JeanBilheux <bilheuxjm@ornl.gov>
2024-06-14 16:55:06 -0400

    crop and options in place. this refs #210

--- notebooks/__code/images_registration_pystackreg/main.py
+++ notebooks/__code/images_registration_pystackreg/main.py
@@ -1,14 +1,35 @@
 import matplotlib.pyplot as plt
 from ipywidgets import interactive
 import ipywidgets as widgets
-from IPython.display import display
+from IPython.display import display, HTML
 from matplotlib.widgets import RectangleSelector
 import numpy as np
 import matplotlib.patches as patches
+from pystackreg import StackReg
 
 from __code.ipywe import fileselector
 from __code._utilities.file import retrieve_list_of_most_dominant_extension_from_folder
 from __code._utilities.images import read_img_stack
+
+
+class AlgoList:
+
+    translation = StackReg.TRANSLATION
+    rigid_body = StackReg.RIGID_BODY
+    scaled_rotation = StackReg.SCALED_ROTATION
+    affine = StackReg.AFFINE
+    bilinear = StackReg.BILINEAR
+
+
+class ReferenceList:
+
+    previous = 'previous'
+    first = 'first'
+    mean = 'mean'
+
+
+def props(cls):
+    return [i for i in cls.__dict__.keys() if i[:1] != '_']
 
 
 class ImagesRegistrationPystackreg:
@@ -34,6 +55,8 @@ class ImagesRegistrationPystackreg:
 
         self.stack = read_img_stack(list_files=self.list_of_files,
                                     ext=ext)
+
+        display(HTML("Data have been loaded!"))
 
     def display_unregistered(self):
 
@@ -67,18 +90,6 @@ class ImagesRegistrationPystackreg:
 
     def crop_unregistered_images(self):
 
-        # def _toggle_selector(event):
-        #     print('Key pressed.')
-        #     if event.key == 't':
-        #         for selector in selectors:
-        #             name = type(selector).__name__
-        #             if selector.active:
-        #                 print(f'{name} deactivated.')
-        #                 selector.set_active(False)
-        #             else:
-        #                 print(f'{name} activated.')
-        #                 selector.set_active(True)
-
         def _select_callback(eclick, erelease):
             """
             Callback for line selection.
@@ -104,8 +115,49 @@ class ImagesRegistrationPystackreg:
             spancoords='pixels',
             interactive=True)
 
-        # Activate the toggling of selection in the figure
-        # It is not needed but a nice addition
-        # fig.canvas.mpl_connect('key_press_event', _toggle_selector)
-        #ax.set_title("Press 't' to turn ON the selection.\n" + ax.get_title());
         ax.set_title("Click and drag to select region to crop")
+
+    def _get_crop_region(self):
+
+        x_corners = self.selector.corners[0]
+        y_corners = self.selector.corners[1]
+
+        x0 = int(x_corners[0])
+        x1 = int(x_corners[2])
+        y0 = int(y_corners[0])
+        y1 = int(y_corners[2])
+
+        return x0, x1, y0, y1
+
+    def perform_cropping(self):
+
+        x0, x1, y0, y1 = self._get_crop_region()
+
+        self.stack_cropped = []
+        for _image in self.stack:
+            _image = np.array(_image)
+            _image_cropped = _image[y0:y1, x0:x1]
+            self.stack_cropped.append(_image_cropped)
+
+        fig, ax = plt.subplots(ncols=1, nrows=1,
+                               num="Cropped images",
+                               figsize=(15, 5))
+
+        image = ax.imshow(self.stack_cropped[0], vmin=0, vmax=1)
+        ax.set_title("First image")
+        cb = plt.colorbar(image, ax=ax)
+
+    def define_parameters(self):
+        label1 = widgets.Label("Types of registration:")
+        list_options = props(AlgoList)
+        self.algo_options = widgets.RadioButtons(options=list_options)
+        col1 = widgets.VBox([label1, self.algo_options])
+
+        label2 = widgets.Label("Image of reference:")
+        list_options_ref = props(ReferenceList)
+        self.reference_options = widgets.RadioButtons(options=list_options_ref)
+        col2 = widgets.VBox([label2, self.reference_options])
+
+        row = widgets.HBox([col1, col2])
+        display(row)
+
