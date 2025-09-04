fdef77499018d2ba4b1fd11e11e4ed9ab2bcd984
JeanBilheux <bilheuxjm@ornl.gov>
2025-09-04 12:29:27 -0400

    Fixed pixi and formatted table after loading tpx3. this refs #261

--- notebooks/__code/timepix3_raw_to_profile_of_roi/main.py
+++ notebooks/__code/timepix3_raw_to_profile_of_roi/main.py
@@ -1,4 +1,4 @@
-# import tdcsophiread as tdc
+import tdcsophiread as tdc
 import os
 import logging
 from IPython.display import display, HTML
@@ -43,6 +43,7 @@ class Timepix3RawToProfileOfRoi:
     def select_tpx3(self):
         if self.debug:
             self.get_file_infos(file_name=config.default_tpx3_file)
+            self.processing_tpx3(file_name=config.default_tpx3_file)
         else:
             self.nexus_ui = fileselector.FileSelectorPanel(
                 instruction="Select TPX3 raw file ...",
@@ -77,15 +78,11 @@ class Timepix3RawToProfileOfRoi:
     def processing_tpx3(self, file_name=None):
 
         logging.info(f"Processing TPX3 file: {os.path.basename(file_name)} ... (be patient!)")
-        display(HTML("Processing TPX3 file ... be patient!"))
         start_time = time.time()
         
-        ## loading tpx3 file
-        # hits_view =  tdc.process_tpx3(file_name, parallel=True)
-        # hits = np.array(hits_view, copy=False)
-
-        # DEBUG ONLY
-        hits = np.arange(10)
+        # loading tpx3 file
+        hits_view =  tdc.process_tpx3(file_name, parallel=True)
+        hits = np.array(hits_view, copy=False)
         self.hits = hits
 
         end_time = time.time()
@@ -94,27 +91,32 @@ class Timepix3RawToProfileOfRoi:
         display(HTML(f"Processing completed in {processing_time:.2f} seconds."))
 
         # display result as a table
-        display(HTML("<h3>TPX3 Processing Results</h3>"))
-        display(HTML("<table>"))
-        display(HTML(f"<tr><th>Infos</th><th>Value</th></tr>"))
-        display(HTML(f"<tr><td>Total hits extracted</td><td>{len(hits)}</td></tr>"))
-        display(HTML(f"<tr><td>X range</td><td>{hits['x'].min()} - {hits['x'].max()}</td></tr>"))
-        display(HTML(f"<tr><td>Y range</td><td>{hits['y'].min()} - {hits['y'].max()}</td></tr>"))
-        
         from_tof = hits['tof'].min() * 25 / 1e6
         to_tof = hits['tof'].max() * 25 / 1e6
-        display(HTML(f"<tr><td>TOF range</td><td>{from_tof:.3f} - {to_tof:.3f} ms</td></tr>"))
-        display(HTML("</table>"))
-        
-        # chips distribution
-        display(HTML("<h3>Chips distribution (hits per chip)</h3>"))
-        display(HTML("<table>"))
+        display(HTML("""
+        <h3>TPX3 Processing Results</h3>
+        <table border="3px solid black" style="border-collapse:collapse;">
+            <tr><th><b>Infos</b></th><th><b>Value</b></th></tr>
+            <tr><td>Total hits extracted</td><td>{}</td></tr>
+            <tr><td>X range</td><td>{} - {}</td></tr>
+            <tr><td>Y range</td><td>{} - {}</td></tr>
+            <tr><td>TOF range (ms)</td><td>{:.3f} - {:.3f}</td></tr>
+        </table>
+        """.format(len(hits), hits['x'].min(), hits['x'].max(), hits['y'].min(), hits['y'].max(),
+                   from_tof, to_tof)))
+
         unique_chips, chip_counts = np.unique(hits["chip_id"], return_counts=True)
+        _text =  '<h3>Chips distribution (hits per chips)</h3> ' + \
+                '<table border="3px solid black" style="border-collapse:collapse;">'
         for chip, count in zip(unique_chips, chip_counts):
             percentage = 100 * count / len(hits)
-            display(HTML(f"<tr><td>Chip {chip}</td><td>{count:,} hits ({percentage:.1f}%)</td></tr>"))
-            display(HTML(f"<tr><td>Chip {chip}</td><td>{count} ({percentage:.1f}%)</td></tr>"))
-        display(HTML("</table>"))
+            _text += f'<tr><td>Chip {chip}</td><td>{count:,} hits ({percentage:.1f}%)</td></tr>'
+        _text += '</table>'
+        display(HTML(_text))
+   
+
+
+
 
     def select_sampling_percentage(self):
         if len(self.hits) > 100_000:
