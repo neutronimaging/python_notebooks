e4bf5d38f336926c2d285f1fb967251aece28cb6
JeanBilheux <bilheuxjm@ornl.gov>
2025-11-10 08:55:49 -0500

    brought back transpose. this refs #292

--- notebooks/__code/normalization_tof/normalization_for_timepix1_timepix3.py
+++ notebooks/__code/normalization_tof/normalization_for_timepix1_timepix3.py
@@ -89,8 +89,10 @@ class StatusMetadata:
 
 
 def _worker(fl):
-    # return (imread(fl).astype(LOAD_DTYPE)).swapaxes(0, 1)
-    return (imread(fl).astype(np.float32))
+#    return (imread(fl).astype(LOAD_DTYPE)).swapaxes(0, 1)
+    return (imread(fl).astype(np.float32)).swapaxes(0, 1)
+
+    #return (imread(fl).astype(np.float32))
 
 
 def load_data_using_multithreading(list_tif: list = None, combine_tof: bool = False) -> np.ndarray:
