5793e3c58a64af2196291d404d85d2f627ed52ee
JeanBilheux <bilheuxjm@ornl.gov>
2025-12-05 11:52:37 -0500

    trying to fix openssl import. this refs #302

--- notebooks/__code/normalization_tof/config.py
+++ notebooks/__code/normalization_tof/config.py
@@ -2,16 +2,29 @@ from __code.resonance_fitting import DetectorType
 
 
 class DEBUG_DATA:
-    ipts = "IPTS-35167"
+    ipts = "IPTS-35207"
     working_dir = f"/SNS/VENUS/{ipts}"
     autoreduce_dir = f"/SNS/VENUS/{ipts}/shared/autoreduce/mcp/images"
     output_folder = f"{working_dir}/shared/processed_data/jean_test"
-    sample_runs_selected = ["Run_14808"]
-    ob_runs_selected = ["Run_14809"]
+    sample_runs_selected = ["Run_13604"]
+    ob_runs_selected = ["Run_13605"]
     dc_runs_selected = []
     roi = [154, 156, 180, 17]
     container_roi = [150, 150, 40, 40]
-    detector_type = DetectorType.tpx3  # timepix1 or timepix3
+    detector_type = DetectorType.tpx1  # timepix1 or timepix3
+
+## timepix3
+# class DEBUG_DATA:
+#     ipts = "IPTS-35167"
+#     working_dir = f"/SNS/VENUS/{ipts}"
+#     autoreduce_dir = f"/SNS/VENUS/{ipts}/shared/autoreduce/mcp/images"
+#     output_folder = f"{working_dir}/shared/processed_data/jean_test"
+#     sample_runs_selected = ["Run_14808"]
+#     ob_runs_selected = ["Run_14809"]
+#     dc_runs_selected = []
+#     roi = [154, 156, 180, 17]
+#     container_roi = [150, 150, 40, 40]
+#     detector_type = DetectorType.tpx3  # timepix1 or timepix3
 
 timepix1_config = {
     "chip1": {"xoffset": 1, "yoffset": 1, "description": "top right chip"},
